3e170774d8120f1d0caa725b0265247cac9cb279
johann-beleites-sonarsource <63855942+johann-beleites-sonarsource@users.noreply.github.com>
2021-05-18 18:15:56 +0200

    Implemented APIs for resolving probable runtime values of expressions, including String interpolation.

---
+++ sonar-kotlin-plugin/src/main/java/org/sonarsource/kotlin/api/ApiExtensions.kt
@@ -0,0 +1,98 @@
+/*
+ * SonarSource SLang
+ * Copyright (C) 2018-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.kotlin.api
+
+import org.jetbrains.kotlin.com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.psi.Call
+import org.jetbrains.kotlin.psi.KtBinaryExpression
+import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
+import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.psi.KtNameReferenceExpression
+import org.jetbrains.kotlin.psi.KtProperty
+import org.jetbrains.kotlin.psi.KtReferenceExpression
+import org.jetbrains.kotlin.psi.KtStringTemplateExpression
+import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.resolve.DescriptorToSourceUtils
+import org.jetbrains.kotlin.resolve.calls.callUtil.getCall
+
+val GET_PROP_WITH_DEFAULT_MATCHER = FunMatcher {
+    type = "java.util.Properties"
+    names = listOf("getProperty")
+    withArguments("kotlin.String", "kotlin.String")
+}
+
+internal fun KtExpression.predictRuntimeStringValue(bindingContext: BindingContext) =
+    predictRuntimeValueExpression(bindingContext).stringValue(bindingContext)
+
+internal fun KtExpression.predictRuntimeStringValueWithSecondaries(bindingContext: BindingContext) =
+    mutableListOf<PsiElement>().let {
+        predictRuntimeValueExpression(bindingContext).stringValue(bindingContext, it) to it
+    }
+
+internal fun KtExpression.predictRuntimeValueExpression(bindingContext: BindingContext): KtExpression =
+    if (this is KtReferenceExpression) {
+        this.extractFromInitializer(bindingContext)
+    } else {
+        getCall(bindingContext)?.predictValueExpression(bindingContext)
+    } ?: this
+
+/**
+ * @param declarations is used to collect all visited declaration for reporting secondary locations
+ */
+private fun KtExpression.stringValue(
+    bindingContext: BindingContext,
+    declarations: MutableList<PsiElement> = mutableListOf(),
+): String? = when (this) {
+    is KtStringTemplateExpression -> {
+        val entries = entries.map { if (it.expression != null) it.expression!!.stringValue(bindingContext) else it.text }
+        if (entries.all { it != null }) entries.joinToString("") else null
+    }
+    is KtNameReferenceExpression -> {
+        val descriptor = bindingContext.get(BindingContext.REFERENCE_TARGET, this)
+        descriptor?.let {
+            val declaration = DescriptorToSourceUtils.descriptorToDeclaration(descriptor)
+            if (declaration is KtProperty && !declaration.isVar) {
+                declarations.add(declaration)
+                declaration.delegateExpressionOrInitializer?.stringValue(bindingContext)
+            } else null
+        }
+    }
+    is KtDotQualifiedExpression -> selectorExpression?.stringValue(bindingContext)
+    is KtBinaryExpression ->
+        if (operationToken == KtTokens.PLUS)
+            left?.stringValue(bindingContext)?.plus(right?.stringValue(bindingContext))
+        else null
+    else -> null
+}
+
+private fun Call.predictValueExpression(bindingContext: BindingContext) =
+    if (GET_PROP_WITH_DEFAULT_MATCHER.matches(this, bindingContext)) {
+        valueArguments[1].getArgumentExpression()
+    } else null
+
+private fun KtReferenceExpression.extractFromInitializer(bindingContext: BindingContext) =
+    bindingContext.get(BindingContext.REFERENCE_TARGET, this)?.let {
+        DescriptorToSourceUtils.descriptorToDeclaration(it) as? KtProperty
+    }?.let { declaration ->
+        if (!declaration.isVar) {
+            declaration.delegateExpressionOrInitializer?.predictRuntimeValueExpression(bindingContext)
+        } else null
+    }
