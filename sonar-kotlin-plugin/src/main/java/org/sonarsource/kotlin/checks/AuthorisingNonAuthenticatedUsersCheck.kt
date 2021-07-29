9ce64d0c2acc8b9f433bf8cfb030b1880ec7829d
Margarita Nedzelska <margarita.nedzelska@sonarsource.com>
2021-07-29 10:34:52 +0200

    Update AuthorisingNonAuthenticatedUsersCheck to use CallAbstractCheck

--- sonar-kotlin-plugin/src/main/java/org/sonarsource/kotlin/checks/AuthorisingNonAuthenticatedUsersCheck.kt
+++ sonar-kotlin-plugin/src/main/java/org/sonarsource/kotlin/checks/AuthorisingNonAuthenticatedUsersCheck.kt
@@ -4,8 +4,9 @@ import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.calls.callUtil.getCall
 import org.jetbrains.kotlin.resolve.calls.callUtil.getType
+import org.jetbrains.kotlin.resolve.calls.model.ResolvedCall
 import org.sonar.check.Rule
-import org.sonarsource.kotlin.api.AbstractCheck
+import org.sonarsource.kotlin.api.CallAbstractCheck
 import org.sonarsource.kotlin.api.ConstructorMatcher
 import org.sonarsource.kotlin.api.FunMatcher
 import org.sonarsource.kotlin.api.SecondaryLocation
@@ -23,34 +24,38 @@ private val KEY_GEN_BUILDER_SET_AUTH_MATCHER = FunMatcher(qualifier = BUILDER, n
 }
 
 @Rule(key = "S6288")
-class AuthorisingNonAuthenticatedUsersCheck : AbstractCheck() {
+class AuthorisingNonAuthenticatedUsersCheck : CallAbstractCheck() {
+    override fun functionsToVisit() = listOf(KEY_GEN_BUILDER_BUILD_MATCHER)
 
-    override fun visitCallExpression(expression: KtCallExpression, context: KotlinFileContext) {
-        val bindingContext = context.bindingContext
-        val call = expression.getCall(bindingContext) ?: return
-        if (KEY_GEN_BUILDER_BUILD_MATCHER.matches(call, bindingContext)) {
-            var receiver = call
-            val secondaryLocations = mutableListOf<SecondaryLocation>()
+    override fun visitFunctionCall(
+        callExpression: KtCallExpression,
+        resolvedCall: ResolvedCall<*>,
+        kotlinFileContext: KotlinFileContext,
+    ) {
+        val bindingContext = kotlinFileContext.bindingContext
+        val call = callExpression.getCall(bindingContext) ?: return
+        var receiver = call
+        val secondaryLocations = mutableListOf<SecondaryLocation>()
 
-            while (!KEY_GEN_BUILDER_MATCHER.matches(receiver, bindingContext)) {
-                val callElement = receiver.callElement as? KtCallExpression ?: return
+        while (!KEY_GEN_BUILDER_MATCHER.matches(receiver, bindingContext)) {
+            val callElement = receiver.callElement as? KtCallExpression ?: return
 
-                if (KEY_GEN_BUILDER_SET_AUTH_MATCHER.matches(receiver, bindingContext)) {
-                    val argumentExpression = receiver.valueArguments[0]?.getArgumentExpression()!!
-                    argumentExpression.getType(bindingContext)?.let {
-                        val argValue = bindingContext.get(BindingContext.COMPILE_TIME_VALUE, argumentExpression)
-                            ?.getValue(it) as? Boolean
-                        if (argValue != false) return
-                        secondaryLocations.add(SecondaryLocation(context.textRange(callElement.calleeExpression!!)))
-                    }
+            if (KEY_GEN_BUILDER_SET_AUTH_MATCHER.matches(receiver, bindingContext)) {
+                val argumentExpression = receiver.valueArguments[0]?.getArgumentExpression()!!
+                argumentExpression.getType(bindingContext)?.let {
+                    val argValue = bindingContext.get(BindingContext.COMPILE_TIME_VALUE, argumentExpression)
+                        ?.getValue(it) as? Boolean
+                    if (argValue != false) return
+                    secondaryLocations.add(SecondaryLocation(kotlinFileContext.textRange(callElement.calleeExpression!!)))
                 }
-
-                receiver = callElement.predictReceiverExpression(bindingContext)
-                    ?.getCall(bindingContext) ?: return
             }
-            context.reportIssue(receiver.calleeExpression!!,
-                "Make sure authorizing non-authenticated users to use this key is safe here.",
-                secondaryLocations)
+
+            receiver = callElement.predictReceiverExpression(bindingContext)
+                ?.getCall(bindingContext) ?: return
         }
+        kotlinFileContext.reportIssue(receiver.calleeExpression!!,
+            "Make sure authorizing non-authenticated users to use this key is safe here.",
+            secondaryLocations)
     }
+
 }
