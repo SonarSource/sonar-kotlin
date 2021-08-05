9e8c4404c6076aa58762e3836d184cbc6df4bd8f
Margarita <margarita.nedzelska@sonarsource.com>
2021-08-05 17:00:09 +0200

    SONARKT-48 Rule S6301: Mobile database encryption keys should not be disclosed

---
+++ kotlin-checks-test-sources/src/main/java/io/realm/RealmConfiguration.java
@@ -0,0 +1,23 @@
+package io.realm;
+
+import org.jetbrains.annotations.NotNull;
+
+public class RealmConfiguration {
+
+  public static class Builder {
+
+    public RealmConfiguration.Builder name(@NotNull String s) {
+      return null;
+    }
+
+    @NotNull
+    public Builder encryptionKey(@NotNull byte[] toByteArray) {
+      return null;
+    }
+
+    @NotNull
+    public Object build() {
+      return null;
+    }
+  }
+}
