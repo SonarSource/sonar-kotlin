2f02cbd16824cf5293764764c3454d1478dbe335
Johann Beleites <63855942+johann-beleites-sonarsource@users.noreply.github.com>
2021-08-17 18:12:30 +0200

    Remove obsolete code from ruling build file. (#68)

--- its/ruling/build.gradle.kts
+++ its/ruling/build.gradle.kts
@@ -8,21 +8,7 @@ sonarqube.isSkipProject = true
 
 tasks.test {
     onlyIf {
-        project.hasProperty("its") ||
-            project.hasProperty("ruling") ||
-            project.hasProperty("ruling-kotlin") ||
-            project.hasProperty("ruling-ruby") ||
-            project.hasProperty("ruling-scala") ||
-            project.hasProperty("ruling-go")
-    }
-    if (project.hasProperty("ruling-kotlin")) {
-        filter { includeTestsMatching("org.sonarsource.slang.SlangRulingTest.test_kotlin*") }
-    } else if (project.hasProperty("ruling-ruby")) {
-        filter { includeTestsMatching("org.sonarsource.slang.SlangRulingTest.test_ruby") }
-    } else if (project.hasProperty("ruling-scala")) {
-        filter { includeTestsMatching("org.sonarsource.slang.SlangRulingTest.test_scala") }
-    } else if (project.hasProperty("ruling-go")) {
-        filter { includeTestsMatching("org.sonarsource.slang.SlangRulingTest.test_go") }
+        project.hasProperty("its") || project.hasProperty("ruling")
     }
     systemProperty("java.awt.headless", "true")
     outputs.upToDateWhen { false }
