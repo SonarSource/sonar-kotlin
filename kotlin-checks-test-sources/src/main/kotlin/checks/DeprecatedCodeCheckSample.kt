3a2ec6222dc4289b02b6549c9c73096a91758c88
Margarita <margarita.nedzelska@sonarsource.com>
2021-08-19 16:47:37 +0200

    Improve issue location
    Co-authored-by: Evgeny Mandrikov <mandrikov@gmail.com>

--- kotlin-checks-test-sources/src/main/kotlin/checks/DeprecatedCodeCheckSample.kt
+++ kotlin-checks-test-sources/src/main/kotlin/checks/DeprecatedCodeCheckSample.kt
@@ -1,7 +1,38 @@
 package checks
 
-  @Deprecated("") // Noncompliant {{Do not forget to remove this deprecated code someday.}}
-//^^^^^^^^^^^^^^^
-class DeprecatedCodeCheckSample
+@Deprecated("") 
+class DeprecatedCodeCheckSample { // Noncompliant {{Do not forget to remove this deprecated code someday.}}
+//    ^^^^^^^^^^^^^^^^^^^^^^^^^
+  
+  @Deprecated("")
+  constructor(s: String) { // Noncompliant
+//^^^^^^^^^^^    
+  }
+  
+  @Deprecated("")
+  val prop: String = "" // Noncompliant
+//    ^^^^
+  
+  var prop2: String = ""
+  @Deprecated("") set // Noncompliant
+//                ^^^    
+  @Deprecated("") get // Noncompliant
+//                ^^^    
+
+}
+
+@Deprecated("")
+fun deprecated() { // Noncompliant
+//  ^^^^^^^^^^  
+  
+}
+
+@Deprecated("")
+annotation class Deprecated2 // Noncompliant
 
 class OK
+
+@Deprecated("")
+typealias KtString = String // Noncompliant
+//        ^^^^^^^^
+
--- sonar-kotlin-plugin/src/main/java/org/sonarsource/kotlin/checks/DeprecatedCodeCheck.kt
+++ sonar-kotlin-plugin/src/main/java/org/sonarsource/kotlin/checks/DeprecatedCodeCheck.kt
@@ -19,7 +19,12 @@
  */
 package org.sonarsource.kotlin.checks
 
+import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.psi.KtAnnotationEntry
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtNamedDeclaration
+import org.jetbrains.kotlin.psi.KtPropertyAccessor
+import org.jetbrains.kotlin.psi.KtSecondaryConstructor
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.sonar.check.Rule
 import org.sonarsource.kotlin.api.AbstractCheck
@@ -27,12 +32,20 @@ import org.sonarsource.kotlin.plugin.KotlinFileContext
 
 @Rule(key = "S1133")
 class DeprecatedCodeCheck : AbstractCheck() {
-
+    
     override fun visitAnnotationEntry(annotationEntry: KtAnnotationEntry, context: KotlinFileContext) {
         val descriptor = context.bindingContext.get(BindingContext.ANNOTATION, annotationEntry)
         if ("kotlin.Deprecated" == descriptor?.fqName?.asString()) {
-            context.reportIssue(annotationEntry, "Do not forget to remove this deprecated code someday.")
+            context.reportIssue(annotationEntry.elementToReport() ?: annotationEntry, "Do not forget to remove this deprecated code someday.")
         }
     }
-
 }
+
+private fun PsiElement.elementToReport(): PsiElement? =
+    when (val parent = parent) {
+        is KtFile -> null
+        is KtSecondaryConstructor -> parent.getConstructorKeyword()
+        is KtPropertyAccessor -> parent.namePlaceholder
+        is KtNamedDeclaration -> parent.nameIdentifier
+        else -> parent.elementToReport()
+    }
