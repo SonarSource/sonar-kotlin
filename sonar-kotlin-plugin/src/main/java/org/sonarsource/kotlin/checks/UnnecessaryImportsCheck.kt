4006621b4e0959acc8c72b67262515074e35abd8
Johann Beleites <johann.beleites@sonarsource.com>
2021-09-01 17:21:18 +0200

    Code quality improvements, removed dependency on fir package

--- sonar-kotlin-plugin/src/main/java/org/sonarsource/kotlin/checks/UnnecessaryImportsCheck.kt
+++ sonar-kotlin-plugin/src/main/java/org/sonarsource/kotlin/checks/UnnecessaryImportsCheck.kt
@@ -19,8 +19,6 @@
  */
 package org.sonarsource.kotlin.checks
 
-import org.jetbrains.kotlin.com.intellij.psi.PsiElement
-import org.jetbrains.kotlin.fir.builder.toBinaryName
 import org.jetbrains.kotlin.kdoc.psi.impl.KDocLink
 import org.jetbrains.kotlin.psi.KtArrayAccessExpression
 import org.jetbrains.kotlin.psi.KtElement
@@ -31,14 +29,14 @@ import org.jetbrains.kotlin.psi.KtOperationReferenceExpression
 import org.jetbrains.kotlin.psi.KtPackageDirective
 import org.jetbrains.kotlin.psi.KtReferenceExpression
 import org.jetbrains.kotlin.psi.KtSimpleNameExpression
+import org.jetbrains.kotlin.psi.KtTreeVisitorVoid
 import org.jetbrains.kotlin.psi.KtUserType
-import org.jetbrains.kotlin.psi.KtVisitorVoid
 import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
-import org.jetbrains.kotlin.psi.psiUtil.isDotSelector
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.calls.callUtil.getResolvedCall
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameOrNull
 import org.jetbrains.kotlin.resolve.descriptorUtil.getImportableDescriptor
+import org.jetbrains.kotlin.types.expressions.OperatorConventions
 import org.sonar.check.Rule
 import org.sonarsource.kotlin.api.AbstractCheck
 import org.sonarsource.kotlin.plugin.KotlinFileContext
@@ -49,60 +47,71 @@ private const val MESSAGE_REDUNDANT = "Remove redundant import."
 @Rule(key = "S1128")
 class UnnecessaryImportsCheck : AbstractCheck() {
 
-    private fun Sequence<PsiElement>.flattenNodes(): Sequence<PsiElement> =
-        this + this.flatMap { it.children.asSequence().flattenNodes() }
-
     override fun visitKtFile(file: KtFile, context: KotlinFileContext) {
 
-        val (references, arrayAccesses, kDocLinks) = collectData(file)
+        val (references, arrayAccesses, kDocLinks) = collectReferences(file)
         val bindingContext = context.bindingContext
         val groupedReferences = references.groupBy { reference ->
             reference.importableSimpleName()
         }
 
-        val arrayAccessesImportsFilter by lazy {
-            getArrayAccessImportsFilter(arrayAccesses, bindingContext)
-        }
+        val arrayAccessesImportsFilter by lazy { getArrayAccessImportsFilter(arrayAccesses, bindingContext) }
 
-        file.importDirectives.filter { imp ->
-            // 1. Filter out & report all imports that import from kotlin.* or the same package as our file
-            if (imp.isImportedImplicitlyAlready(file.packageDirective?.name)) {
-                imp.importedReference?.let { context.reportIssue(it, MESSAGE_REDUNDANT) }
-                false
-            } else true
-        }.groupBy {
-            it.importedName?.asString()
-        }.filterKeys { simpleName ->
-            // 2. Discard all imports that could be relevant for KDocs. This is done a bit fuzzy.
-            simpleName != null && simpleName !in kDocLinks
-        }.flatMap { (simpleName, importsWithSameName) ->
-            // 3. Discard all imports that are used in references throughout the code. With binding context if possible,
-            // otherwise we over-estimate
-            groupedReferences[simpleName]?.let { relevantReferences ->
-                var relevantImports = importsWithSameName
-                for (ref in relevantReferences) {
-                    val refName = bindingContext.get(BindingContext.REFERENCE_TARGET, ref)?.getImportableDescriptor()?.fqNameOrNull()
-                        ?: return@flatMap emptyList() // Discard all: over-estimate, resulting in less FPs and more FNs without binding ctx
-                    relevantImports = relevantImports.filter { it.importedFqName != refName }
-                    if (relevantImports.isEmpty()) break
-                }
-                relevantImports
-            } ?: importsWithSameName
-        }.filter {
-            // 4. Filter 'get' and 'set' imports
-            arrayAccessesImportsFilter(it)
-        }.forEach { importDirective ->
-            // We could not find any usages for anything remaining at this point. Hence, report!
-            importDirective.importedReference?.let{ context.reportIssue(it, MESSAGE_UNUSED) }
-        }
+        analyzeImports(file, groupedReferences, kDocLinks, arrayAccessesImportsFilter, context)
     }
 
-    private fun collectData(file: KtFile) =
+    private fun analyzeImports(
+        file: KtFile,
+        groupedReferences: Map<String?, List<KtReferenceExpression>>,
+        kDocLinks: Collection<String>,
+        arrayAccessesImportsFilter: (KtImportDirective) -> Boolean,
+        context: KotlinFileContext
+    ) = file.importDirectives.filter { imp ->
+        // 1. Filter out & report all imports that import from kotlin.* or the same package as our file
+        if (imp.isImportedImplicitlyAlready(file.packageDirective?.name)) {
+            imp.importedReference?.let { context.reportIssue(it, MESSAGE_REDUNDANT) }
+            false
+        } else true
+    }.groupBy {
+        it.importedName?.asString()
+    }.filterKeys { simpleName ->
+        // 2. Discard all imports that could be relevant for KDocs. This is done a bit fuzzy.
+        simpleName != null && simpleName !in kDocLinks
+    }.flatMap { (simpleName, importsWithSameName) ->
+        // 3. Discard all imports that are used in references throughout the code. With binding context if possible,
+        // otherwise we over-estimate
+        groupedReferences[simpleName]?.let { relevantReferences ->
+            filterImportsWithSameSimpleNameByReferences(importsWithSameName, relevantReferences, context)
+        } ?: importsWithSameName
+    }.filter {
+        // 4. Filter 'get' and 'set' imports
+        arrayAccessesImportsFilter(it)
+    }.forEach { importDirective ->
+        // We could not find any usages for anything remaining at this point. Hence, report!
+        importDirective.importedReference?.let { context.reportIssue(it, MESSAGE_UNUSED) }
+    }
+
+    private fun filterImportsWithSameSimpleNameByReferences(
+        importsWithSameName: List<KtImportDirective>,
+        relevantReferences: List<KtReferenceExpression>,
+        context: KotlinFileContext
+    ): List<KtImportDirective> {
+        var relevantImports = importsWithSameName
+        for (ref in relevantReferences) {
+            val refName = context.bindingContext.get(BindingContext.REFERENCE_TARGET, ref)?.getImportableDescriptor()?.fqNameOrNull()
+                ?: return emptyList() // Discard all: over-estimate, resulting in less FPs and more FNs without binding ctx
+            relevantImports = relevantImports.filter { it.importedFqName != refName }
+            if (relevantImports.isEmpty()) break
+        }
+        return relevantImports
+    }
+
+    private fun collectReferences(file: KtFile) =
         file.children.asSequence().filter {
             it !is KtPackageDirective && it !is KtImportList
         }.let { relevantTopLevelChildren ->
             DataCollector(file).apply {
-                relevantTopLevelChildren.flattenNodes()
+                relevantTopLevelChildren
                     .filterIsInstance<KtElement>()
                     .forEach { ktElement ->
                         ktElement.accept(this)
@@ -128,7 +137,7 @@ class UnnecessaryImportsCheck : AbstractCheck() {
         }
 }
 
-private class DataCollector(val file: KtFile) : KtVisitorVoid() {
+private class DataCollector(val file: KtFile) : KtTreeVisitorVoid() {
 
     data class DataCollectionResult(
         val references: Collection<KtReferenceExpression>,
@@ -149,11 +158,15 @@ private class DataCollector(val file: KtFile) : KtVisitorVoid() {
         }?.let {
             references.add(it)
         }
+        recurse(expression)
     }
 
     override fun visitArrayAccessExpression(expression: KtArrayAccessExpression) {
         arrayAccesses.add(expression)
+        recurse(expression)
     }
+
+    private fun recurse(element: KtElement) = super.visitElement(element)
 
     fun collectFromKDocComments(comments: Sequence<KDocLink>) {
         comments.forEach { kDocLinks.add(it.getLinkText().substringBefore('.')) }
@@ -168,7 +181,9 @@ private fun KtImportDirective.isImportedImplicitlyAlready(containingPackage: Str
 
 private fun KtReferenceExpression.importableSimpleName() =
     when (this) {
-        is KtOperationReferenceExpression -> operationSignTokenType?.toBinaryName()?.asString() ?: getReferencedName()
+        is KtOperationReferenceExpression -> operationSignTokenType
+            ?.let { (OperatorConventions.BINARY_OPERATION_NAMES[it] ?: OperatorConventions.UNARY_OPERATION_NAMES[it])?.asString() }
+            ?: getReferencedName()
         is KtSimpleNameExpression -> getReferencedName()
         else -> null
     }
