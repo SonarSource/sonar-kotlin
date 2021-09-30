fbd6fc6f291a2aae8a7cac8918eb760722ec49a0
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2021-09-30 14:55:29 +0200

    SONARKT-125 S1143: jump statements should not occur in finally blocks

---
+++ kotlin-checks-test-sources/src/main/kotlin/checks/ReturnInFinallyCheckSample.kt
@@ -0,0 +1,171 @@
+package checks
+
+class ReturnInFinallyCheckSample {
+
+    class NonCompliant {
+
+        fun returnInFinally() {
+            try {
+                throw RuntimeException()
+            } finally {
+                return  // Noncompliant {{Remove this return statement from this finally block.}}
+            }
+        }
+
+        fun throwInFinally() {
+            try {
+                throw RuntimeException()
+            } finally {
+                throw RuntimeException() // Noncompliant {{Remove this throw statement from this finally block.}}
+            }
+        }
+
+        fun breakInFinally() {
+            while (true) {
+                try {
+                    throw RuntimeException()
+                } finally {
+                    break // Noncompliant {{Remove this break statement from this finally block.}}
+                }
+            }
+
+            do {
+                try {
+                    throw RuntimeException()
+                } finally {
+                    break // Noncompliant {{Remove this break statement from this finally block.}}
+                }
+            } while (true)
+
+            val letters = "Hello, World!".chars()
+            for (ignored in letters) {
+                try {
+                    throw RuntimeException()
+                } finally {
+                    break // Noncompliant {{Remove this break statement from this finally block.}}
+                }
+            }
+
+            outer@ while (true) {
+                try {
+                    throw RuntimeException()
+                } finally {
+                    inner@ do {
+                        break@outer // Noncompliant {{Remove this break statement from this finally block.}}
+                    } while (true)
+                }
+            }
+        }
+
+        fun continueInFinallyInWhile() {
+            while (true) {
+                try {
+                    throw RuntimeException()
+                } finally {
+                    continue // Noncompliant {{Remove this continue statement from this finally block.}}
+                }
+            }
+        }
+
+        fun continueInFinallyInDoWhile() {
+            do {
+                try {
+                    throw RuntimeException()
+                } finally {
+                    continue // Noncompliant {{Remove this continue statement from this finally block.}}
+                }
+            } while (true)
+        }
+
+        fun continueInFinallyInFor() {
+            val letters = "Hello, World!".chars()
+            for (ignored in letters) {
+                try {
+                    throw RuntimeException()
+                } finally {
+                    continue // Noncompliant {{Remove this continue statement from this finally block.}}
+                }
+            }
+        }
+
+        fun continueToOuterLabelInFinally() {
+            outer@ while (true) {
+                try {
+                    throw RuntimeException()
+                } finally {
+                    inner@ do {
+                        continue@outer // Noncompliant {{Remove this continue statement from this finally block.}}
+                    } while (true)
+                }
+            }
+        }
+    }
+
+    class Compliant {
+        fun breakInForInFinally() {
+            try {
+                throw RuntimeException()
+            } finally {
+                for (ignored in "Hello, World!".chars()) {
+                    break // Compliant
+                }
+            }
+        }
+
+        fun breakInDoWhileInFinally() {
+            try {
+                throw RuntimeException()
+            } finally {
+                do {
+                    break // Compliant
+                } while (true)
+            }
+        }
+
+        fun breakInWhileInFinally() {
+            try {
+                throw RuntimeException()
+            } finally {
+                while (true) {
+                    break // Compliant
+                }
+            }
+        }
+
+        fun breakToLabelInFinally() {
+            while (true) {
+                try {
+                    throw RuntimeException()
+                } finally {
+                    inner@ do {
+                        break@inner // Compliant
+                    } while (true)
+                }
+            }
+        }
+
+        fun continueToInnerLabelInFinally() {
+            while (true) {
+                try {
+                    throw RuntimeException()
+                } finally {
+                    inner@ do {
+                        continue@inner // Compliant
+                    } while (true)
+                }
+            }
+        }
+
+        fun returnInLambdaInFinally() {
+            while (true) {
+                try {
+                    throw RuntimeException()
+                } finally {
+                    listOf(1, 2, 3).forEach {
+                        if (it == 0) return // Compliant
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ sonar-kotlin-plugin/src/main/java/org/sonarsource/kotlin/checks/ReturnInFinallyCheck.kt
@@ -0,0 +1,145 @@
+/*
+ * SonarSource Kotlin
+ * Copyright (C) 2018-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.kotlin.checks
+
+import org.jetbrains.kotlin.com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.psi.KtBreakExpression
+import org.jetbrains.kotlin.psi.KtContinueExpression
+import org.jetbrains.kotlin.psi.KtDoWhileExpression
+import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.psi.KtExpressionWithLabel
+import org.jetbrains.kotlin.psi.KtFinallySection
+import org.jetbrains.kotlin.psi.KtForExpression
+import org.jetbrains.kotlin.psi.KtLabelReferenceExpression
+import org.jetbrains.kotlin.psi.KtLabeledExpression
+import org.jetbrains.kotlin.psi.KtLambdaExpression
+import org.jetbrains.kotlin.psi.KtReturnExpression
+import org.jetbrains.kotlin.psi.KtThrowExpression
+import org.jetbrains.kotlin.psi.KtTreeVisitorVoid
+import org.jetbrains.kotlin.psi.KtWhileExpression
+import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
+import org.sonar.check.Rule
+import org.sonarsource.kotlin.api.AbstractCheck
+import org.sonarsource.kotlin.plugin.KotlinFileContext
+
+private fun extractLabelTextOrNull(label: PsiElement): String? {
+    when (label) {
+        is KtLabelReferenceExpression ->
+            return label.text.trim('@')
+    }
+    return null
+}
+
+/**
+ * Checks if the label in the element points to an unexpected label
+ */
+private fun isEscapingTheBlock(element: KtExpressionWithLabel, expectedLabels: List<String>): Boolean {
+    if (element.labelQualifier == null) {
+        return false
+    }
+    val label = extractLabelTextOrNull(element.labelQualifier!!.firstChild) ?: return false
+    return label !in expectedLabels
+}
+
+private fun buildReportMessage(element: KtElement) =
+    "Remove this %s statement from this finally block.".format(element.toString().lowercase())
+
+@Rule(key = "S1143")
+class ReturnInFinallyCheck : AbstractCheck() {
+
+    override fun visitFinallySection(finallySection: KtFinallySection, kotlinFileContext: KotlinFileContext) {
+        val report: (KtElement) -> Unit = {
+            kotlinFileContext.reportIssue(it, buildReportMessage(it))
+        }
+
+        val visitor = FinallyBlockVisitor(report)
+        finallySection.accept(visitor)
+        visitor.jumpsToLabel
+            .filter { isEscapingTheBlock(it, visitor.labelsInFinallyBlock) }
+            .forEach(report)
+
+        finallySection
+            .collectDescendantsOfType<KtThrowExpression>()
+            .forEach(report)
+    }
+
+    private class FinallyBlockVisitor(private val report: (KtElement) -> Unit) : KtTreeVisitorVoid() {
+        var loopDepthCounter = 0
+        var lambdaDepthCounter = 0
+        val labelsInFinallyBlock = ArrayList<String>()
+        val jumpsToLabel = ArrayList<KtExpressionWithLabel>()
+
+        override fun visitLabeledExpression(expression: KtLabeledExpression) {
+            if (expression.labelQualifier != null) {
+                extractLabelTextOrNull(expression.labelQualifier!!.firstChild)?.let { labelsInFinallyBlock.add(it) }
+            }
+            super.visitLabeledExpression(expression)
+        }
+
+        override fun visitLambdaExpression(lambdaExpression: KtLambdaExpression) {
+            lambdaDepthCounter++
+            super.visitLambdaExpression(lambdaExpression)
+            lambdaDepthCounter--
+        }
+
+        override fun visitDoWhileExpression(expression: KtDoWhileExpression) {
+            loopDepthCounter++
+            super.visitDoWhileExpression(expression)
+            loopDepthCounter--
+        }
+
+        override fun visitForExpression(expression: KtForExpression) {
+            loopDepthCounter++
+            super.visitForExpression(expression)
+            loopDepthCounter--
+        }
+
+        override fun visitWhileExpression(expression: KtWhileExpression) {
+            loopDepthCounter++
+            super.visitWhileExpression(expression)
+            loopDepthCounter--
+        }
+
+        override fun visitBreakExpression(expression: KtBreakExpression) {
+            if (loopDepthCounter == 0) {
+                report(expression)
+            } else if (expression.labelQualifier != null) {
+                jumpsToLabel.add(expression)
+            }
+            super.visitBreakExpression(expression)
+        }
+
+        override fun visitContinueExpression(expression: KtContinueExpression) {
+            if (loopDepthCounter == 0) {
+                report(expression)
+            } else if (expression.labelQualifier != null) {
+                jumpsToLabel.add(expression)
+            }
+            super.visitContinueExpression(expression)
+        }
+
+        override fun visitReturnExpression(expression: KtReturnExpression) {
+            if (lambdaDepthCounter == 0) {
+                report(expression)
+            }
+            super.visitReturnExpression(expression)
+        }
+    }
+}
