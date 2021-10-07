83b4e3808d835d8d388a6eb8b4612db1404342a7
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2021-10-07 17:39:23 +0200

    SONARKT-127 Add test cases without semantics

--- sonar-kotlin-plugin/src/test/java/org/sonarsource/kotlin/checks/EqualsOverriddenWithArrayFieldCheckTest.kt
+++ sonar-kotlin-plugin/src/test/java/org/sonarsource/kotlin/checks/EqualsOverriddenWithArrayFieldCheckTest.kt
@@ -19,4 +19,4 @@
  */
 package org.sonarsource.kotlin.checks
 
-class EqualsOverriddenWithArrayFieldCheckTest : CheckTest(EqualsOverriddenWithArrayFieldCheck())
+class EqualsOverriddenWithArrayFieldCheckTest : CheckTestWithNoSemantics(EqualsOverriddenWithArrayFieldCheck())
