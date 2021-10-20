a9f41b0cd35812441b0938c9b5d1f6b776f7d0b5
Margarita <margarita.nedzelska@sonarsource.com>
2021-10-20 14:36:59 +0200

    SONARKT-179 Improve regex range to Kotlin file range translation precesion

---
+++ kotlin-checks-test-sources/src/main/kotlin/checks/ReportCharacterClassRegexDummyCheckSample.kt
@@ -0,0 +1,12 @@
+package checks
+
+fun characterClasses() {
+
+      Regex("[a-z]abc[A-Z]abc[b-d]abc[0-9]") // Noncompliant {{Character class found}}
+//           ^^^^^   ^^^^^<  ^^^^^<  ^^^^^<
+    
+    
+    """[0-9]x\nxx[a-z]x\nxx[_-z]""".toRegex() // Noncompliant {{Character class found}}
+//     ^^^^^>    ^^^^^>    ^^^^^>   ^^^^^^^ 
+    
+}
