a3bcecce7087480572d8571f03b71df60fd99b2b
Johann Beleites <63855942+johann-beleites-sonarsource@users.noreply.github.com>
2021-10-27 10:08:40 +0200

    SONARKT-155 S5850: Alternatives in regular expressions should be grouped when used with anchors (#108)

--- kotlin-checks-test-sources/src/main/kotlin/checks/UnpredictableSecureRandomSaltCheckSample.kt
+++ kotlin-checks-test-sources/src/main/kotlin/checks/UnpredictableSecureRandomSaltCheckSample.kt
@@ -2,29 +2,31 @@ package checks
 
 import java.security.SecureRandom
 
-fun noncompliant() {
-    val sr1 = SecureRandom()
-    sr1.setSeed(123456L) // Noncompliant {{Change this seed value to something unpredictable, or remove the seed.}}
-//              ^^^^^^^
-    sr1.setSeed("foo".toByteArray()) // Noncompliant
-    val v1 = sr1.nextInt()
+class UnpredictableSecureRandomSaltCheckSample {
+    fun noncompliant() {
+        val sr1 = SecureRandom()
+        sr1.setSeed(123456L) // Noncompliant {{Change this seed value to something unpredictable, or remove the seed.}}
+//                  ^^^^^^^
+        sr1.setSeed("foo".toByteArray()) // Noncompliant
+        val v1 = sr1.nextInt()
 
-    val sr2 = SecureRandom("abcdefghijklmnop".toByteArray(charset("us-ascii"))) // Noncompliant
-    val v2 = sr2.nextInt()
+        val sr2 = SecureRandom("abcdefghijklmnop".toByteArray(charset("us-ascii"))) // Noncompliant
+        val v2 = sr2.nextInt()
 
-    // Noncompliant@+3
-    val predictableByteArray = ByteArray(100)
-//                             ^^^^^^^^^^^^^^>
-    SecureRandom(predictableByteArray)
-//               ^^^^^^^^^^^^^^^^^^^^
-}
+        // Noncompliant@+3
+        val predictableByteArray = ByteArray(100)
+//                                 ^^^^^^^^^^^^^^>
+        SecureRandom(predictableByteArray)
+//                   ^^^^^^^^^^^^^^^^^^^^
+    }
 
-fun compliant() {
-    val sr = SecureRandom()
-    val v = sr.nextInt()
+    fun compliant() {
+        val sr = SecureRandom()
+        val v = sr.nextInt()
 
-    val byteArray = ByteArray(50)
-    sr.nextBytes(byteArray)
-    SecureRandom(byteArray)
-    sr.setSeed(sr.nextLong())
+        val byteArray = ByteArray(50)
+        sr.nextBytes(byteArray)
+        SecureRandom(byteArray)
+        sr.setSeed(sr.nextLong())
+    }
 }
---
+++ sonar-kotlin-plugin/src/main/java/org/sonarsource/kotlin/checks/AnchorPrecedenceCheck.kt
@@ -0,0 +1,33 @@
+/*
+ * SonarSource Kotlin
+ * Copyright (C) 2018-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.kotlin.checks
+
+import org.sonar.check.Rule
+import org.sonarsource.analyzer.commons.regex.RegexParseResult
+import org.sonarsource.analyzer.commons.regex.finders.AnchorPrecedenceFinder
+import org.sonarsource.kotlin.api.regex.AbstractRegexCheck
+import org.sonarsource.kotlin.api.regex.RegexContext
+
+@Rule(key = "S5850")
+class AnchorPrecedenceCheck : AbstractRegexCheck() {
+    override fun visitRegex(regex: RegexParseResult, regexContext: RegexContext) {
+        AnchorPrecedenceFinder(regexContext::reportIssue).visit(regex)
+    }
+}
