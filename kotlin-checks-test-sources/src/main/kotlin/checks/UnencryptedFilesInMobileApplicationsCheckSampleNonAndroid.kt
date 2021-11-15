fd929d6e614ad83a67c646d24aa4388845f8d315
margarita-nedzelska-sonarsource <70522623+margarita-nedzelska-sonarsource@users.noreply.github.com>
2021-11-15 16:35:54 +0100

    SONARKT-149 S6300 should cover more methods that write to files (#134)

--- kotlin-checks-test-sources/src/main/kotlin/checks/UnencryptedFilesInMobileApplicationsCheckSampleNonAndroid.kt
+++ kotlin-checks-test-sources/src/main/kotlin/checks/UnencryptedFilesInMobileApplicationsCheckSampleNonAndroid.kt
@@ -1,11 +1,21 @@
 package checks
 
 import java.io.File
+import java.io.FileOutputStream
+import java.io.FileWriter
+import java.nio.file.Files
+import java.nio.file.Path
 
 class UnencryptedFilesInMobileApplicationsCheckSampleNonAndroid {
     fun f(fileContent: String) {
         val targetFile = File("my_sensitive_data.txt")
         targetFile.writeText(fileContent) // Ok, not in an Android context
         targetFile.appendBytes(fileContent.toByteArray()) // Ok, not in an Android context
+
+        val fileWriter = FileWriter("my_sensitive_data.txt")
+
+        val fileOutputStream = FileOutputStream("my_sensitive_data.txt")
+
+        Files.write(Path.of("my_sensitive_data.txt"), fileContent.toByteArray())
     }
 }
