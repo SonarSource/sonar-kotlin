b0c3c41005149a422de886498ac48382f41fb7d9
Leonardo Pilastri <leonardo.pilastri@sonarsource.com>
2022-11-04 14:21:17 +0100

    Restructured custom line number test unit

--- sonar-kotlin-plugin/src/test/java/org/sonarsource/kotlin/checks/TextBlocksInComplexExpressionsCheckTest.kt
+++ sonar-kotlin-plugin/src/test/java/org/sonarsource/kotlin/checks/TextBlocksInComplexExpressionsCheckTest.kt
@@ -22,13 +22,17 @@ package org.sonarsource.kotlin.checks
 import org.junit.jupiter.api.Test
 import org.sonarsource.kotlin.verifier.KotlinVerifier
 
-internal class TextBlocksInComplexExpressionsCheckTest : CheckTest(TextBlocksInComplexExpressionsCheck())
+internal class TextBlocksInComplexExpressionsCheckTest : CheckTest(TextBlocksInComplexExpressionsCheck()) {
 
-@Test
-fun test() {
-    val check = TextBlocksInComplexExpressionsCheck()
-    check.linesNumber = 1
-    KotlinVerifier(check) {
-        fileName = "TextBlocksInComplexExpressionsCheckSampleSingleLines.kt"
-    }.verify()
+    @Test
+    fun `custom linesNumber rule property is effectively enforced`() {
+        val check = TextBlocksInComplexExpressionsCheck()
+        check.linesNumber = 1
+        KotlinVerifier(check) {
+            fileName = "TextBlocksInComplexExpressionsCheckSampleSingleLines.kt"
+        }.verify()
+    }
+
 }
+
+
