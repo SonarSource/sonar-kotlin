8d498876351ab883e9491659bd2eb793a0acb261
Margarita Nedzelska <margarita.nedzelska@sonarsource.com>
2022-11-04 10:20:33 +0100

    SONARKT-230 Upgrade to Detekt v1.22.0-RC2 rule definition

--- sonar-kotlin-plugin/src/test/java/org/sonarsource/kotlin/externalreport/detekt/DetektRulesDefinitionTest.kt
+++ sonar-kotlin-plugin/src/test/java/org/sonarsource/kotlin/externalreport/detekt/DetektRulesDefinitionTest.kt
@@ -36,7 +36,7 @@ internal class DetektRulesDefinitionTest {
         assertThat(repository!!.name()).isEqualTo("detekt")
         assertThat(repository.language()).isEqualTo("kotlin")
         assertThat(repository.isExternal).isTrue
-        assertThat(repository.rules().size).isEqualTo(200)
+        assertThat(repository.rules().size).isEqualTo(203)
 
         val classNaming = repository.rule("ClassNaming")
         assertThat(classNaming).isNotNull
