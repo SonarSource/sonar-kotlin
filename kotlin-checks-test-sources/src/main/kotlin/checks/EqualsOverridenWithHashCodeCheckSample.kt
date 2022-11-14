234ed1d449812a23e32805fd32fd1ae41f58ca39
leonardo-pilastri-sonarsource <115481625+leonardo-pilastri-sonarsource@users.noreply.github.com>
2022-11-14 16:24:06 +0100

    [SONARKT-129] Fixed FP for abstract methods for S1206 (#226)

--- kotlin-checks-test-sources/src/main/kotlin/checks/EqualsOverridenWithHashCodeCheckSample.kt
+++ kotlin-checks-test-sources/src/main/kotlin/checks/EqualsOverridenWithHashCodeCheckSample.kt
@@ -61,6 +61,31 @@ class EqualsOverridenWithHashCodeCheckSample {
         }
     }
 
+
 }
 
+abstract class AmbiguousParent {
+    abstract override fun equals(other: Any?): Boolean
+}
 
+abstract class AmbiguousParent4 {
+    abstract override fun hashCode(): Int
+}
+
+abstract class AmbiguousParent2 {
+    abstract override fun hashCode(): Int
+
+    override fun equals(other: Any?): Boolean {
+        return super.equals(other)
+    }
+}
+
+abstract class AmbiguousParent3 {
+
+    override fun hashCode(): Int { // Noncompliant
+        return super.hashCode()
+    }
+    fun equals(other: String?): Boolean {
+        return super.equals(other)
+    }
+}
