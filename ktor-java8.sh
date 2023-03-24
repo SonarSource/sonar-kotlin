56c84699267a0a2f6e16755b92ce24f85d5ddab6
Margarita Nedzelska <margarita.nedzelska@sonarsource.com>
2023-03-24 18:35:09 +0100

    Update Ktor project to run it on Java 17

--- ktor-java8.sh
+++
@@ -1,8 +0,0 @@
-mkdir .jdk
-curl -L "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_linux_hotspot_8u292b10.tar.gz" -o .jdk/jdk.tar.gz
-tar -xzf .jdk/jdk.tar.gz -C .jdk --strip-components 1
-.jdk/bin/java -version
-export JAVA_HOME=$PWD/.jdk
-cd its/sources/kotlin/ktor
-./gradlew build -x test
-cd ../../../../
