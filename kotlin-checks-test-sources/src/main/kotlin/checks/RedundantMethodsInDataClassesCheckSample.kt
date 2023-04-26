7101a59f65d55990fb1f3cd3ee16a7b173489cf3
Irina Batinic <irina.batinic@sonarsource.com>
2023-04-26 11:14:09 +0200

    SONARKT-119 Refactoring and support of Arrays.hashCode case

--- kotlin-checks-test-sources/src/main/kotlin/checks/RedundantMethodsInDataClassesCheckSample.kt
+++ kotlin-checks-test-sources/src/main/kotlin/checks/RedundantMethodsInDataClassesCheckSample.kt
@@ -1,6 +1,6 @@
 package checks
 
-import java.util.Objects
+import java.util.*
 
 class RedundantMethodsInDataClassesCheckSample {
 
@@ -10,8 +10,9 @@ class RedundantMethodsInDataClassesCheckSample {
             return other is Person1 && other.name == name && other.age == age
         }
 
-        override fun hashCode() = Objects.hash(name, age) // Noncompliant {{Remove this redundant method which is the same as a default one.}}
+        override fun hashCode() // Noncompliant {{Remove this redundant method which is the same as a default one.}}
 //                   ^^^^^^^^
+                = Objects.hash(name, age)
     }
 
     data class Person2(val name: String, val age: Int) // Compliant
@@ -37,12 +38,12 @@ class RedundantMethodsInDataClassesCheckSample {
             return other is Person5 && other.name == name && age.dec() == age
         }
 
-        override fun hashCode() = Objects.hash(age, name) // Noncompliant
+        override fun hashCode() = Objects.hash(age, name) + Objects.hash(name, age)// Compliant
     }
 
     data class Person6(val name: String, val age: Int) {
         override fun equals(other: Any?): Boolean { // Compliant
-            return other is Person6 && other.name == name && age.dec() == age
+            return other is Person6 && other.name == name && other.age == age && 1 != 1
         }
 
         override fun hashCode() = Objects.hash(name.lowercase(), age.dec()) // Compliant
@@ -89,9 +90,8 @@ class RedundantMethodsInDataClassesCheckSample {
     data class Person12(val name: String, val age: Int) {
         val a = 1
         val b = ""
-        override fun equals(other: Any?): Boolean { // Compliant
-            return other is Person11 && other.b == name && other.age == age
-        }
+        override fun equals(other: Any?): Boolean = other is Person12 && other.b == name && other.age == age
+
 
         override fun hashCode() = hash() // Compliant
 
@@ -99,6 +99,53 @@ class RedundantMethodsInDataClassesCheckSample {
             return 1
         }
 
+    }
+
+    data class Person13(val name: String, val age: Int) {
+        override fun equals(other: Any?): Boolean { // Compliant
+            val x = other is Person13 && other.name == name && other.age == age
+            return true
+        }
+
+        override fun hashCode(): Int { // Compliant
+            val x = Objects.hash(name, age)
+            if (true) {
+                return x + 1
+            }
+            return Objects.hash(name, age)
+        }
+    }
+
+    data class Person14(val name: String, val age: Int) {
+        override fun equals(other: Any?): Boolean { // Compliant
+            if (true) {
+                return other is Person14 && other.name == name && other.age == age
+            } else {
+                return other is Person14 && other.name == name && other.age == age
+            }
+            val x = other is Person14 && other.name == name && other.age == age
+            return true
+        }
+
+        override fun hashCode(): Int { // Compliant
+            return 31 * name.hashCode() + Integer.hashCode(age)
+        }
+    }
+
+    data class Person15(val name: String, val age: Int) {
+        override fun equals(other: Any?): Boolean { // Compliant
+            return other is Person1 && other.name == name && other.age == age
+        }
+
+        override fun hashCode() = Arrays.hashCode(arrayOf(name, age)) // Noncompliant
+    }
+
+    data class Person16(val name: String, val age: Int) {
+        override fun equals(other: Any?): Boolean { // Compliant
+            return other is Person1 && other.name == name && other.age == age
+        }
+
+        override fun hashCode() = Arrays.hashCode(arrayOf(name)) // Compliant
     }
 
 }
--- sonar-kotlin-plugin/src/main/java/org/sonarsource/kotlin/checks/RedundantMethodsInDataClassesCheck.kt
+++ sonar-kotlin-plugin/src/main/java/org/sonarsource/kotlin/checks/RedundantMethodsInDataClassesCheck.kt
@@ -20,32 +20,27 @@
 package org.sonarsource.kotlin.checks
 
 import org.jetbrains.kotlin.lexer.KtTokens
-import org.jetbrains.kotlin.psi.KtBinaryExpression
-import org.jetbrains.kotlin.psi.KtCallExpression
-import org.jetbrains.kotlin.psi.KtClass
-import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
-import org.jetbrains.kotlin.psi.KtExpression
-import org.jetbrains.kotlin.psi.KtNameReferenceExpression
-import org.jetbrains.kotlin.psi.KtNamedFunction
-import org.jetbrains.kotlin.psi.KtParameter
+import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
 import org.jetbrains.kotlin.psi.psiUtil.findDescendantOfType
+import org.jetbrains.kotlin.resolve.BindingContext
 import org.sonar.check.Rule
-import org.sonarsource.kotlin.api.ANY_TYPE
-import org.sonarsource.kotlin.api.AbstractCheck
-import org.sonarsource.kotlin.api.EQUALS_METHOD_NAME
-import org.sonarsource.kotlin.api.FunMatcher
-import org.sonarsource.kotlin.api.HASHCODE_METHOD_NAME
+import org.sonarsource.kotlin.api.*
 import org.sonarsource.kotlin.plugin.KotlinFileContext
 
-private val equalsMatcher = FunMatcher {
+private val EQUALS_MATCHER = FunMatcher {
     name = EQUALS_METHOD_NAME
     withArguments(ANY_TYPE)
 }
-private val hashCodeMatcher = FunMatcher {
+private val HASHCODE_MATCHER = FunMatcher {
     name = HASHCODE_METHOD_NAME
     withNoArguments()
 }
+
+private val OBJECT_ARRAY_HASH_MATCHER = listOf(
+        FunMatcher(qualifier = "java.util.Objects", name = "hash"),
+        FunMatcher(qualifier = "java.util.Arrays", name = "hashCode")
+)
 
 @Rule(key = "S6207")
 class RedundantMethodsInDataClassesCheck : AbstractCheck() {
@@ -53,53 +48,95 @@ class RedundantMethodsInDataClassesCheck : AbstractCheck() {
     private val issueMessage = "Remove this redundant method which is the same as a default one."
 
     override fun visitClass(klass: KtClass, context: KotlinFileContext) {
-        if (!klass.isData()) {
-            return
-        }
+        if (!klass.isData()) return
+
+        // if class is data class it always contains constructor
         val klassParameters = klass.primaryConstructor!!.valueParameters
         var equalsMethod: KtNamedFunction? = null
         var hashCodeMethod: KtNamedFunction? = null
 
         klass.body?.functions?.forEach {
             when {
-                equalsMatcher.matches(it, context.bindingContext) -> equalsMethod = it
-                hashCodeMatcher.matches(it, context.bindingContext) -> hashCodeMethod = it
+                EQUALS_MATCHER.matches(it, context.bindingContext) -> equalsMethod = it
+                HASHCODE_MATCHER.matches(it, context.bindingContext) -> hashCodeMethod = it
             }
         }
 
         equalsMethod?.let {
-            if (it.equalsHasDefaultImpl(klassParameters, it.valueParameters)) {
+            if (it.equalsHasDefaultImpl(klassParameters, it.valueParameters, klass.name)) {
                 context.reportIssue(it.nameIdentifier!!, issueMessage)
             }
         }
 
         hashCodeMethod?.let {
-            if (it.hashCodeHasDefaultImpl(klassParameters)) {
+            if (it.hashCodeHasDefaultImpl(klassParameters, context.bindingContext)) {
                 context.reportIssue(it.nameIdentifier!!, issueMessage)
             }
         }
     }
 
-    private fun KtNamedFunction.hashCodeHasDefaultImpl(klassParameters: List<KtParameter>): Boolean {
-        val hashExpression =
-            this.findDescendantOfType<KtCallExpression> { (it.calleeExpression as KtNameReferenceExpression).getReferencedName() == "hash" }
-        // hash expression in this case is always not null
-        val arguments = hashExpression?.valueArgumentList?.collectDescendantsOfType<KtNameReferenceExpression>() ?: return false
-        if (klassParameters.size != arguments.size) {
-            return false
+    private fun KtNamedFunction.hashCodeHasDefaultImpl(klassParameters: List<KtParameter>, bindingContext: BindingContext): Boolean {
+        if (hasBlockBody()) {
+            val returnExpressions = collectDescendantsOfType<KtReturnExpression>()
+            if (returnExpressions.size > 1 || returnExpressions.isEmpty()) return false
+
+            return checkHashExpression(returnExpressions[0], bindingContext, klassParameters)
+        } else {
+            return checkHashExpression(this, bindingContext, klassParameters)
         }
-        arguments.forEach { findParameter(it, klassParameters) ?: return false }
+    }
+
+    private fun checkHashExpression(expression: KtExpression, bindingContext: BindingContext, klassParameters: List<KtParameter>): Boolean {
+        val callExpressions = expression.collectDescendantsOfType<KtCallExpression> {
+            OBJECT_ARRAY_HASH_MATCHER.any { e ->
+                e.matches(it, bindingContext)
+            }
+        }
+        if (callExpressions.size > 1 || callExpressions.isEmpty()) return false
+
+        if ((callExpressions[0].calleeExpression as KtNameReferenceExpression).getReferencedName() == "hash") {
+            if (callExpressions[0].valueArguments.size != klassParameters.size) return false
+            callExpressions[0].valueArguments.forEach {
+                findParameter(it.getArgumentExpression(), klassParameters) ?: return false
+            }
+        } else {
+            val arguments = callExpressions[0].valueArguments[0].collectDescendantsOfType<KtValueArgumentList>()
+            if (arguments.isEmpty() || arguments.size > 1) return false
+
+            if (arguments[0].arguments.size != klassParameters.size) return false
+            arguments[0].arguments.forEach {
+                findParameter(it.getArgumentExpression(), klassParameters) ?: return false
+            }
+        }
         return true
     }
 
     private fun KtNamedFunction.equalsHasDefaultImpl(
-        klassParameters: List<KtParameter>,
-        methodParameters: MutableList<KtParameter>,
+            klassParameters: List<KtParameter>,
+            methodParameters: MutableList<KtParameter>,
+            className: String?
     ): Boolean {
-        val equalsExpressions = this.collectDescendantsOfType<KtBinaryExpression> { it.operationToken == KtTokens.EQEQ }
-        if (equalsExpressions.size != klassParameters.size) {
-            return false
+        if (hasBlockBody()) {
+            val returnExpressions = collectDescendantsOfType<KtReturnExpression>()
+            if (returnExpressions.size > 1 || returnExpressions.isEmpty()) return false
+            return checkEqualsExpression(returnExpressions[0], klassParameters, methodParameters, className)
+        } else {
+            val expression = findDescendantOfType<KtBinaryExpression>() ?: return false
+            return checkEqualsExpression(expression, klassParameters, methodParameters, className)
         }
+    }
+
+    private fun checkEqualsExpression(expression: KtExpression, klassParameters: List<KtParameter>, methodParameters: MutableList<KtParameter>, className: String?): Boolean {
+        val notEqualsExpressions = expression.collectDescendantsOfType<KtBinaryExpression> { it.operationToken == KtTokens.EXCLEQ }
+        if (notEqualsExpressions.isNotEmpty()) return false
+
+        val isExpressions = expression.collectDescendantsOfType<KtIsExpression>()
+        if (isExpressions.size > 1 || isExpressions.isEmpty()) return false
+        if (isExpressions[0].isNegated || isExpressions[0].typeReference?.nameForReceiverLabel() != className) return false
+
+        val equalsExpressions = expression.collectDescendantsOfType<KtBinaryExpression> { it.operationToken == KtTokens.EQEQ }
+        if (equalsExpressions.size != klassParameters.size) return false
+
         val map = mutableMapOf<KtParameter, Boolean>()
         klassParameters.forEach {
             map[it] = false
@@ -108,47 +145,41 @@ class RedundantMethodsInDataClassesCheck : AbstractCheck() {
         equalsExpressions.forEach {
             val left = it.left
             val right = it.right
-            checkExpression(left, right, klassParameters, methodParameters, map)
-            checkExpression(right, left, klassParameters, methodParameters, map)
+            checkIfExpressionHasParameter(left, right, klassParameters, methodParameters, map)
+            checkIfExpressionHasParameter(right, left, klassParameters, methodParameters, map)
         }
+
         return !map.values.contains(false)
     }
 
-    private fun checkExpression(
-        first: KtExpression?,
-        second: KtExpression?,
-        klassParameters: List<KtParameter>,
-        methodParameters: MutableList<KtParameter>,
-        map: MutableMap<KtParameter, Boolean>,
+    private fun checkIfExpressionHasParameter(
+            first: KtExpression?,
+            second: KtExpression?,
+            klassParameters: List<KtParameter>,
+            methodParameters: MutableList<KtParameter>,
+            map: MutableMap<KtParameter, Boolean>,
     ) {
         val parameter = findParameter(first, klassParameters) ?: return
-        when {
-            checkDotExpression(second, parameter, methodParameters) -> {
-                map[parameter] = true
-            }
-        }
+        if (checkDotExpression(second, parameter, methodParameters))
+            map[parameter] = true
     }
 
     private fun findParameter(
-        expression: KtExpression?,
-        klassParameters: List<KtParameter>,
-    ): KtParameter? {
-        if (expression is KtNameReferenceExpression) {
-            return klassParameters.find { klassParameter -> klassParameter.name == expression.getReferencedName() }
-        }
-        return null
-    }
+            expression: KtExpression?,
+            klassParameters: List<KtParameter>,
+    ): KtParameter? =
+            if (expression is KtNameReferenceExpression)
+                klassParameters.find { klassParameter -> klassParameter.name == expression.getReferencedName() }
+            else null
 
     private fun checkDotExpression(
-        expression: KtExpression?,
-        klassParameter: KtParameter,
-        methodParameters: MutableList<KtParameter>,
-    ): Boolean {
-        if (expression is KtDotQualifiedExpression) {
-            return ((expression.receiverExpression as KtNameReferenceExpression).getReferencedName() == methodParameters[0].name
-                && (expression.selectorExpression as KtNameReferenceExpression).getReferencedName() == klassParameter.name)
-        }
-        return false
-    }
+            expression: KtExpression?,
+            klassParameter: KtParameter,
+            methodParameters: MutableList<KtParameter>,
+    ): Boolean =
+            if (expression is KtDotQualifiedExpression) {
+                ((expression.receiverExpression as KtNameReferenceExpression).getReferencedName() == methodParameters[0].name
+                        && (expression.selectorExpression as KtNameReferenceExpression).getReferencedName() == klassParameter.name)
+            } else false
 
 }
