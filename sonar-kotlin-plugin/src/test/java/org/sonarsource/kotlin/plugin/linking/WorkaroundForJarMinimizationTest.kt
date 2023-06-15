f2b17501de483aa21a25e5a6773cfcb82d71f7d5
Marco Kaufmann <marco.kaufmann@sonarsource.com>
2023-06-15 16:56:47 +0200

    - fixed JAR minimization

--- sonar-kotlin-plugin/src/test/java/org/sonarsource/kotlin/plugin/linking/WorkaroundForJarMinimizationTest.kt
+++ sonar-kotlin-plugin/src/test/java/org/sonarsource/kotlin/plugin/linking/WorkaroundForJarMinimizationTest.kt
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package org.sonarsource.kotlin.api.frontend
+package org.sonarsource.kotlin.plugin.linking
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
