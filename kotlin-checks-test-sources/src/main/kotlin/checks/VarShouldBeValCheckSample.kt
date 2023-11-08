033702b00677ee0f979a2d998f4b4b1813fa53be
erwan-serandour-sonarsource <erwan.serandour@sonarsource.com>
2023-11-08 15:36:37 +0100

    add first implementation of rule S3353

---
+++ kotlin-checks-test-sources/src/main/kotlin/checks/VarShouldBeValCheckSample.kt
@@ -0,0 +1,82 @@
+package checks
+
+import kotlin.math.max
+
+class VarShouldBeValCheckSample {
+    var notUsed = "not used" // compliant, not a local variable
+    var x = "x" // compliant
+        set(value){
+            x = value
+        }
+
+    class Address {
+        var street: String = "baker" // compliant
+    }
+
+    fun assignmentOperators(): Unit {
+        var j = 0 // Noncompliant
+        var x = 0 // compliant
+        x = 1
+        var y = 0 // compliant
+        y += 1
+        var i = 0 // compliant
+        i -= 1
+        var z = 0 // compliant
+        z++
+        var w = 0 // compliant
+        w--
+        var k = 0 // compliant
+        ++k
+        var l = 0 // compliant
+        --l
+        var v = 0 // compliant
+        v *= 2
+        var u = 0 // compliant
+        u/= 2
+        var t = 0 // compliant
+        t %= 2
+        var (a, b) = Pair(0, 1) // not supported for now, should we ?
+        a = 1
+        var c = 0 // compliant
+        c = 1 as Int
+    }
+
+    fun assignmentExpressions(): Unit {
+        val numbers = mutableListOf(1, 2, 3, 4)
+        var x = 0 // compliant
+        (x) = 1
+        var y = 0 // compliant
+        ((y)) += 1
+        val z = object {
+            var z = 0
+        }
+        z.z = 10
+
+        numbers[0] = 2
+
+    }
+
+    fun resizeNonCompliant(): Int {
+        var newLength = max(16, 2) // Noncompliant {{Replace the keyword `var` with `val`.}}
+        return newLength
+    }
+
+    fun nested(): Int {
+        var shadowed = 0 // Noncompliant
+        var z = 0 // compliant
+        fun nestedFun(x : Int): Unit {
+           var shadowed = 1 // compliant
+           shadowed = 2
+           z = 2
+        }
+
+        return shadowed
+    }
+
+
+    fun resizeCompliant(): Int {
+        val newLength = max(16, 2) // Compliant
+        return newLength
+    }
+
+}
---
+++ sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/VarShouldBeValCheck.kt
@@ -0,0 +1,91 @@
+/*
+ * SonarSource Kotlin
+ * Copyright (C) 2018-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.kotlin.checks
+
+import org.jetbrains.kotlin.com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.psi.KtBinaryExpression
+import org.jetbrains.kotlin.psi.KtBlockExpression
+import org.jetbrains.kotlin.psi.KtOperationExpression
+import org.jetbrains.kotlin.psi.KtProperty
+import org.jetbrains.kotlin.psi.KtReferenceExpression
+import org.jetbrains.kotlin.psi.KtUnaryExpression
+import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
+import org.jetbrains.kotlin.psi.psiUtil.findDescendantOfType
+import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
+import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.resolve.BindingContext.REFERENCE_TARGET
+import org.jetbrains.kotlin.resolve.DescriptorToSourceUtils
+import org.sonar.check.Rule
+import org.sonarsource.kotlin.api.checks.AbstractCheck
+import org.sonarsource.kotlin.api.frontend.KotlinFileContext
+
+@Rule(key = "S3353")
+class VarShouldBeValCheck : AbstractCheck() {
+    override fun visitProperty(property: KtProperty, context: KotlinFileContext) {
+        if (property.isLocal && property.isVar) {
+            val bindingContext = context.bindingContext
+            val block = property.getParentOfType<KtBlockExpression>(false)!!
+            val assignments = collectAssignmentTo(block, property, bindingContext)
+
+            if (assignments.isEmpty()) {
+                context.reportIssue(property, """Replace the keyword `var` with `val`.""")
+            }
+        }
+    }
+
+    private fun collectAssignmentTo(block: PsiElement, variable: KtProperty, bindingContext: BindingContext): List<KtOperationExpression> {
+        return block
+            .collectDescendantsOfType<KtOperationExpression> { expr -> isAssignment(expr) }
+            .filter { assignment ->
+                when (assignment) {
+                    is KtBinaryExpression -> getReference(assignment).let { isReferencingVar(variable, it, bindingContext) } ?: false
+                    is KtUnaryExpression -> isReferencingVar(variable, assignment.baseExpression as KtReferenceExpression, bindingContext)
+                    // I don't know how to cover it
+                    else -> false
+                }
+            }
+    }
+
+
+    private fun isAssignment(expr: KtOperationExpression): Boolean {
+        return when (expr.operationReference.getReferencedNameElementType()) {
+            KtTokens.EQ -> true
+            KtTokens.PLUSEQ -> true
+            KtTokens.MINUSEQ -> true
+            KtTokens.MULTEQ -> true
+            KtTokens.DIVEQ -> true
+            KtTokens.PERCEQ -> true
+            KtTokens.PLUSPLUS -> true
+            KtTokens.MINUSMINUS -> true
+            else -> false
+        }
+    }
+
+    private fun getReference(expr: KtBinaryExpression): KtReferenceExpression {
+        return expr.left!!.findDescendantOfType<KtReferenceExpression>()!!
+    }
+
+    private fun isReferencingVar(variable: KtProperty, reference: KtReferenceExpression, bindingContext: BindingContext): Boolean {
+        val definition =
+            bindingContext[REFERENCE_TARGET, reference]?.let { DescriptorToSourceUtils.descriptorToDeclaration(it) as? KtProperty }
+        return variable === definition
+    }
+}
