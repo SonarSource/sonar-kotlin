54506e1299c5ab4f4a19e02038fa0f80cb7559a5
erwan-serandour-sonarsource <erwan.serandour@sonarsource.com>
2023-11-27 14:00:45 +0100

    SONARKT-301 - Implement rule S6524: collection should be immutable (#390)

---
+++ kotlin-checks-test-sources/src/main/kotlin/checks/CollectionShouldBeImmutableCheckSampleNoSemantics.kt
@@ -0,0 +1,215 @@
+package checks
+
+class CollectionShouldBeImmutableCheckSampleNoSemantics {
+    fun MutableCollection<Int>.doSomething(): Unit {}
+
+    //let also apply run with
+    fun nonCompliant(
+        x: MutableList<Int>,
+        y: MutableSet<String>,
+        z: MutableMap<Int, Int>,
+        a: MutableList<Int>,
+        b: MutableList<Int>,
+        c: MutableList<Int>,
+    ): Unit {
+        baz(x.toList().toMutableList())
+        baz(doNothing(x).toMutableList())
+        doNothing(x)
+        x.reduce { acc, it -> acc + it }
+        x + 1
+        x.size
+        for (i in x) {
+            println(i)
+        }
+        x.foo()
+        id(x + listOf(1, 2, 3))
+        y.contains("a")
+        y.map { it + "a" }
+        z[1]
+        doNothing(x, y, z)
+        a.let { it.size }
+        a.let { it.also { with(it, { this.size }) } }
+        Pair(1, b.reduce { acc, i -> i + acc })
+        id(c)
+    }
+
+    fun List<Int>.toList(): List<Int> = this // compliant
+    fun baz(list : MutableList<Int>): Unit {}
+
+    fun List<Int>.foo(): Unit {} // Compliant
+    fun doNothing(a : List<Int>, b : Set<String>, c : Map<Int,Int>): Unit {}
+    fun doNothing(a : List<Int>): List<Int> { return a } // Compliant
+
+    fun <A>id(a : A): A = a // Compliant
+
+    fun compliantScopedFunctions(
+        a: MutableList<Int>, // Compliant
+        b: MutableList<Int>, // Compliant
+        c: MutableList<Int>, // Compliant
+        d: MutableList<Int>, // Compliant
+        e: MutableList<Int>, // Compliant
+        f: MutableList<Int>, // Compliant
+        crazy: MutableList<Int>,
+    ): Unit { // Compliant
+        a.let { it.add(1) }
+        b.also { it.add(1) }
+        c.apply { add(1) }
+        d.run { add(1) }
+        with(e) { add(1) }
+        f.let { it.let { it.let { it.add(1) } } }
+        crazy.let { it.also { it.apply { it.run { with(this) { it.add(1) } } } } }
+    }
+
+    fun MutableList<Int>.doSomething2(): MutableList<Int> { return this }
+
+    fun compliantFunctionsCalledOn(
+        a: MutableList<Int>, // Compliant
+        b: MutableList<Int>, // Compliant
+        c: List<Int>, // Compliant
+        d: MutableList<Int>, // Compliant
+        e: MutableMap<Int, Int>, // Compliant
+        f: MutableList<Int>, // Compliant
+        g: MutableList<Int>, // Compliant
+        h: MutableSet<Int>, // Compliant
+        i: MutableList<Int>, // Compliant
+        j: MutableList<Int>, // Compliant
+        k: MutableList<Int>, // Compliant
+        l: MutableList<Int>, // Compliant
+        m: MutableList<Int>, // Compliant
+        n: MutableList<Int>, // Compliant
+    ): Unit {
+        a.add(1)
+        b.iterator()
+        c.map { it + 1}
+        d.add(1)
+        e.remove(1)
+        f.doSomething()
+        g.doSomething2()
+        h.doSomething()
+        ((i)).doSomething()
+        j.doSomething2().doSomething2().doSomething2()
+        when (k) {
+            is MutableList<Int> -> k.doSomething()
+        }
+        l!!.doSomething()
+        m?.doSomething()
+        if(true){n}else{n}.doSomething()
+    }
+
+
+    fun compliantBinaryExprCalledOn(
+        c: List<Int>, // Compliant
+        d: MutableList<Int>, // Compliant
+        e: MutableList<Int>, // Compliant
+        f: MutableList<Int>, // Compliant
+        g: MutableList<Int>, // Compliant
+    ): Unit {
+        c[0]
+        d += 1
+        e -= 1
+        f[0] = 1
+        (g)[0] = 1
+    }
+
+
+
+    fun compliantCalledInFun(
+        a: MutableList<Int>, // Compliant
+        b: MutableList<Int>, // Compliant
+        c: MutableList<Int>, // Compliant
+        d: MutableList<Int>, // Compliant
+        e: MutableList<Int>, // Compliant
+        f: MutableList<Int> // Compliant
+    ): Unit {
+        foo_(a)
+        foo2(b)
+        foo_(((c)))
+        foo_(d!!)
+        foo_(if(true){e}else{e})
+        Pair(f, 1)
+    }
+
+    fun foo_(x : MutableList<Int>): Unit {}
+    fun foo2(x : MutableCollection<Int>): Unit {}
+
+
+    fun nonCompliantParameter(list: MutableList<Int>): Int {
+        return list.reduce { acc, it -> acc + it}
+    }
+
+    fun compliantParameter(list: List<Int>): Int { // Compliant
+        return list.reduce { acc, it -> acc + it}
+    }
+
+    fun MutableList<Int>.compliantDelegate(): Int { // Compliant
+        this.add(1)
+        return reduce { acc, it -> acc + it }
+    }
+
+
+    fun MutableList<Int>.compliantDelegate2(): Unit { // Compliant
+        foo_(this)
+    }
+
+    fun MutableList<Int>.compliantDelegate3(): Unit { // Compliant
+        foo_(this)
+        add(1)
+    }
+
+    fun MutableList<Int>.noncompliantDelegate(): Int {
+        return reduce { acc, it -> acc + it}
+    }
+
+
+    class AMutableCollections : MutableCollection<Int> {
+        override val size: Int
+            get() = TODO()
+
+        override fun contains(element: Int): Boolean {
+            TODO()
+        }
+
+        override fun containsAll(elements: Collection<Int>): Boolean {
+            TODO()
+        }
+
+        override fun isEmpty(): Boolean {
+            TODO()
+        }
+
+        override fun add(element: Int): Boolean {
+            TODO()
+        }
+
+        override fun addAll(elements: Collection<Int>): Boolean {
+            TODO()
+        }
+
+        override fun clear() {
+            TODO()
+        }
+
+        override fun iterator(): MutableIterator<Int> {
+            TODO()
+        }
+
+        override fun remove(element: Int): Boolean {
+            TODO()
+        }
+
+        override fun removeAll(elements: Collection<Int>): Boolean {
+            TODO()
+        }
+
+        override fun retainAll(elements: Collection<Int>): Boolean {
+            TODO()
+        }
+    }
+
+    fun id(x : AMutableCollections): AMutableCollections = x // compliant, for now don't know how to check that is subtype of MutableCollection
+
+
+    fun foo(configure: (MutableMap<String, Any?>) -> Unit): Unit { // compliant
+    }
+
+}
---
+++ sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/CollectionShouldBeImmutableCheck.kt
@@ -0,0 +1,184 @@
+/*
+ * SonarSource Kotlin
+ * Copyright (C) 2018-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.kotlin.checks
+
+import org.jetbrains.kotlin.com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.descriptors.CallableDescriptor
+import org.jetbrains.kotlin.js.descriptorUtils.getKotlinTypeFqName
+import org.jetbrains.kotlin.psi.KtBinaryExpression
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
+import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.psi.KtNameReferenceExpression
+import org.jetbrains.kotlin.psi.KtNamedDeclaration
+import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.psi.KtQualifiedExpression
+import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
+import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.resolve.BindingContext.DECLARATION_TO_DESCRIPTOR
+import org.jetbrains.kotlin.resolve.calls.util.getResolvedCall
+import org.jetbrains.kotlin.resolve.descriptorUtil.isExtension
+import org.jetbrains.kotlin.types.KotlinType
+import org.sonar.check.Rule
+import org.sonarsource.kotlin.api.checks.AbstractCheck
+import org.sonarsource.kotlin.api.checks.determineTypeAsString
+import org.sonarsource.kotlin.api.frontend.KotlinFileContext
+
+@Rule(key = "S6524")
+class CollectionShouldBeImmutableCheck : AbstractCheck() {
+    private val mutableCollections =
+        setOf(
+            "kotlin.collections.MutableList",
+            "kotlin.collections.MutableSet",
+            "kotlin.collections.MutableMap",
+            "kotlin.collections.MutableCollection"
+        )
+
+
+    override fun visitNamedFunction(function: KtNamedFunction, context: KotlinFileContext) {
+        val bindingContext = context.bindingContext
+
+        val referencesToMutableCollections = collectReferenceToMutatedCollections(function, bindingContext)
+        val mutatedNames = referencesToMutableCollections.map { it.getReferencedName() }.toSet()
+        val mutatedDeclarations =
+            referencesToMutableCollections.mapNotNull { it.getResolvedCall(bindingContext)?.resultingDescriptor?.original }.toSet()
+
+        reportPropertiesAndParameters(function, mutatedDeclarations, bindingContext, context)
+
+        if (function.receiverTypeReference?.determineTypeAsString(bindingContext) in mutableCollections) {
+            reportForExtensionFunction(function, bindingContext, mutatedNames, context)
+        }
+    }
+
+    private fun reportPropertiesAndParameters(
+        function: KtNamedFunction,
+        mutatedDeclarations: Set<CallableDescriptor>,
+        bindingContext: BindingContext,
+        context: KotlinFileContext,
+    ) {
+        function.valueParameters
+            .filter { it.isMutableCollection(bindingContext) }
+            .filter { it.isNotReferenced(mutatedDeclarations, bindingContext) }
+            .forEach { context.reportIssue(it, "Make this collection immutable.") }
+    }
+
+    private fun KtNamedDeclaration.isMutableCollection(bindingContext: BindingContext): Boolean =
+        this.determineTypeAsString(bindingContext) in mutableCollections
+
+    private fun KtNamedDeclaration.isNotReferenced(
+        mutatedDeclarations: Set<CallableDescriptor>,
+        bindingContext: BindingContext,
+    ): Boolean {
+        val descriptor = bindingContext[DECLARATION_TO_DESCRIPTOR, this]
+        //descriptor is never null because we need the descriptor to know if we will consider the declaration
+        //no need to return false if descriptor is null
+        return !mutatedDeclarations.contains(descriptor)
+    }
+
+    private fun reportForExtensionFunction(
+        function: KtNamedFunction,
+        bindingContext: BindingContext,
+        mutatedNames: Set<String>,
+        context: KotlinFileContext,
+    ) {
+        val functionsCalledOnThis = function
+            .collectDescendantsOfType<KtCallExpression> { it.noReceiver() }
+
+        if (functionsCalledOnThis.none { it.mutatesCollection(bindingContext) } && "this" !in mutatedNames) {
+            context.reportIssue(function.receiverTypeReference!!.navigationElement, "Make this collection immutable.")
+        }
+    }
+
+    private fun KtCallExpression.noReceiver(): Boolean {
+        return this.parent !is KtQualifiedExpression
+    }
+
+    private fun collectReferenceToMutatedCollections(
+        function: KtNamedFunction,
+        bindingContext: BindingContext,
+    ): List<KtNameReferenceExpression> {
+        val mutablePropertiesReceiver: List<KtNameReferenceExpression> = function
+            .collectDescendantsOfType<KtQualifiedExpression> { it.mutatesCollection(bindingContext) }
+            .flatMap { findReferenceExpressions(it.receiverExpression) }
+
+        val mutablePropertiesInBinaryExpression: List<KtNameReferenceExpression> = function
+            .collectDescendantsOfType<KtBinaryExpression> { it.mutatesCollection(bindingContext) }
+            .flatMap { findReferenceExpressions(it.left!!) }
+
+        val mutablePropertiesInFunctionCalls: List<KtNameReferenceExpression> = function
+            .collectDescendantsOfType<KtCallExpression>()
+            .flatMap { call ->
+                val functionDescriptor = call.getResolvedCall(bindingContext)?.resultingDescriptor
+                val parameterTypes = functionDescriptor?.valueParameters
+                if (parameterTypes != null) {
+                    val fullyQualifiedTypes = parameterTypes.map { it.type.getKotlinTypeFqName(false) }
+                    call.valueArguments.zip(fullyQualifiedTypes).filter { it.second in mutableCollections }.map { it.first }
+                } else {
+                    call.valueArguments
+                }
+            }
+            .flatMap { findReferenceExpressions(it.getArgumentExpression()!!) }
+
+        return mutablePropertiesReceiver + mutablePropertiesInBinaryExpression + mutablePropertiesInFunctionCalls
+    }
+
+    private fun findReferenceExpressions(
+        expression: KtExpression,
+    ): List<KtNameReferenceExpression> {
+        return expression.collectDescendantsOfType<KtNameReferenceExpression>(::considerElement) { true }
+    }
+
+    private fun considerElement(element: PsiElement): Boolean {
+        return when (element) {
+            is KtCallExpression -> false
+            is KtDotQualifiedExpression -> false
+            else -> true
+        }
+    }
+
+    private fun KtQualifiedExpression.mutatesCollection(bindingContext: BindingContext): Boolean {
+        return if (this.selectorExpression is KtCallExpression) {
+            val selectorExpression = this.selectorExpression as KtCallExpression
+            selectorExpression.mutatesCollection(bindingContext)
+        } else {
+            false
+        }
+    }
+
+    private fun KtElement.mutatesCollection(bindingContext: BindingContext): Boolean {
+        val functionDescriptor = this.getResolvedCall(bindingContext)?.resultingDescriptor
+        val receiverType = functionDescriptor?.receiverType()
+        return if (receiverType?.constructor?.declarationDescriptor != null) {
+            receiverType.getKotlinTypeFqName(false) in mutableCollections
+        } else {
+            true
+        }
+    }
+
+    private fun CallableDescriptor.receiverType(): KotlinType? {
+        return if (this.isExtension) {
+            this.extensionReceiverParameter?.type
+        } else {
+            this.dispatchReceiverParameter?.type
+        }
+    }
+
+}
