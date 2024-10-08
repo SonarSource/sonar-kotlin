f3ce6505391e54fd9a94e174fbf3f554ad392d1d
Evgeny Mandrikov <mandrikov@gmail.com>
2024-10-08 15:40:36 +0200

    (WIP) UnencryptedFilesInMobileApplicationsCheck

--- sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/UnencryptedFilesInMobileApplicationsCheck.kt
+++ sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/UnencryptedFilesInMobileApplicationsCheck.kt
@@ -19,8 +19,8 @@
  */
 package org.sonarsource.kotlin.checks
 
+import org.jetbrains.kotlin.analysis.api.resolution.KaFunctionCall
 import org.jetbrains.kotlin.psi.KtCallExpression
-import org.jetbrains.kotlin.resolve.calls.model.ResolvedCall
 import org.sonar.check.Rule
 import org.sonarsource.kotlin.api.checks.CallAbstractCheck
 import org.sonarsource.kotlin.api.checks.ConstructorMatcher
@@ -38,8 +38,7 @@ class UnencryptedFilesInMobileApplicationsCheck : CallAbstractCheck() {
         FunMatcher(qualifier = "java.nio.file.Files", name = "write"),
     )
 
-    // TODO easy
-    override fun visitFunctionCall(callExpression: KtCallExpression, resolvedCall: ResolvedCall<*>, kotlinFileContext: KotlinFileContext) {
+    override fun visitFunctionCall(callExpression: KtCallExpression, resolvedCall: KaFunctionCall<*>, kotlinFileContext: KotlinFileContext) {
         if (kotlinFileContext.isInAndroid()) {
             kotlinFileContext.reportIssue(callExpression.calleeExpression!!, MESSAGE)
         }
