1a2d0f6e964fe34d7649f8a9cb0314e434c44387
Evgeny Mandrikov <mandrikov@gmail.com>
2024-10-10 00:26:44 +0200

    revert revert (WIP) TODO IsInstanceMethodCheck

--- sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/IsInstanceMethodCheck.kt
+++ sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/IsInstanceMethodCheck.kt
@@ -82,8 +82,12 @@ class IsInstanceMethodCheck : CallAbstractCheck() {
     private fun isJavaClassKeyword(expr: KtExpression?): Boolean =
         (expr is KtNameReferenceExpression) && (expr.getReferencedName() in JAVA_CLASS_KEYWORDS)
 
-    // TODO easy
-    private fun KtReferenceExpression.isClass(ctx: KotlinFileContext) =
-        ctx.bindingContext[BindingContext.REFERENCE_TARGET, this] is ClassDescriptor
+    // TODO fails in K2 mode
+    private fun KtReferenceExpression.isClass(ctx: KotlinFileContext): Boolean {
+        val expression = this
+        analyze {
+            return expression.mainReference.resolveToSymbol() is KaClassSymbol
+        }
+    }
 
 }
