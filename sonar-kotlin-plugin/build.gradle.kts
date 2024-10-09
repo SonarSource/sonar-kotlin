38c10244d34a4be5f15cd202bf15c1e8507995fe
Evgeny Mandrikov <138671+Godin@users.noreply.github.com>
2024-10-09 12:30:27 +0200

    SONARKT-399 Fix shadowJar configuration
    This was overlooked during replacement of
    "kotlin-compiler-embeddable" by "kotlin-compiler"
    in d7a5cfd5b0a6d96fd23e25711cd2db5eca931c35.

--- sonar-kotlin-plugin/build.gradle.kts
+++ sonar-kotlin-plugin/build.gradle.kts
@@ -96,10 +96,10 @@ val javadocJar = tasks.javadocJar
 tasks.shadowJar {
     minimize {}
     exclude("META-INF/native/**/*jansi*")
-    exclude("org/jetbrains/kotlin/org/jline/**")
-    exclude("org/jetbrains/kotlin/net/jpountz/**")
+    exclude("org/jline/**")
+    exclude("net/jpountz/**")
     doLast {
-        enforceJarSizeAndCheckContent(shadowJar.get().archiveFile.get().asFile, 38_100_000L, 38_600_000L)
+        enforceJarSizeAndCheckContent(shadowJar.get().archiveFile.get().asFile, 37_500_000L, 38_000_000L)
     }
 }
 
