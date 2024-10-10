e6847c18da2a12892252e035e23a9d3d140f7634
Evgeny Mandrikov <mandrikov@gmail.com>
2024-10-10 22:13:01 +0200

    (WIP) add comment

--- sonar-kotlin-plugin-it/build.gradle.kts
+++ sonar-kotlin-plugin-it/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
 
 val shadowJar = project(":sonar-kotlin-plugin").tasks.named("shadowJar")
 
+// TODO look at https://github.com/JetBrains/kotlin/commit/719addaa51bd52d2a8f52ba706f4caf2ed58fec0#diff-c0dfa6bc7a8685217f70a860145fbdf416d449eaff052fa28352c5cec1a98c06L535-L538
+// as a way to hide fat jar from searches
+
 dependencies {
     implementation(files(shadowJar))
     testImplementation(testLibs.junit.jupiter)
