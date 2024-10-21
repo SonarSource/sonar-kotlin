8f494f1cd58a276b1c6267f76af53dce4ec0140e
Evgeny Mandrikov <mandrikov@gmail.com>
2024-10-21 15:04:19 +0200

    (WIP) SuspendingFunCallerDispatcherCheck

--- sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/SuspendingFunCallerDispatcherCheck.kt
+++ sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/SuspendingFunCallerDispatcherCheck.kt
@@ -19,6 +19,10 @@
  */
 package org.sonarsource.kotlin.checks
 
+import org.jetbrains.kotlin.analysis.api.resolution.KaFunctionCall
+import org.jetbrains.kotlin.analysis.api.resolution.singleFunctionCallOrNull
+import org.jetbrains.kotlin.analysis.api.resolution.symbol
+import org.jetbrains.kotlin.analysis.api.symbols.KaNamedFunctionSymbol
 import org.jetbrains.kotlin.backend.common.descriptors.isSuspend
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
@@ -28,34 +32,39 @@ import org.jetbrains.kotlin.resolve.calls.model.ResolvedCall
 import org.sonar.check.Rule
 import org.sonarsource.kotlin.api.checks.CallAbstractCheck
 import org.sonarsource.kotlin.api.checks.FUNS_ACCEPTING_DISPATCHERS
-import org.sonarsource.kotlin.api.frontend.K1only
 import org.sonarsource.kotlin.api.frontend.KotlinFileContext
+import org.sonarsource.kotlin.visiting.analyze
 
-@K1only("easy?")
 @Rule(key = "S6311")
 class SuspendingFunCallerDispatcherCheck : CallAbstractCheck() {
     override val functionsToVisit = FUNS_ACCEPTING_DISPATCHERS
 
-    // TODO easy?
     override fun visitFunctionCall(
         callExpression: KtCallExpression,
-        resolvedCall: ResolvedCall<*>,
+        resolvedCall: KaFunctionCall<*>,
+//        resolvedCall: ResolvedCall<*>,
         kotlinFileContext: KotlinFileContext,
-    ) {
+    ) = analyze {
         val bindingContext = kotlinFileContext.bindingContext
 
-        val arguments = resolvedCall.valueArgumentsByIndex
-        if (arguments.isNullOrEmpty()
-            || arguments[0] == null
-            || arguments[0] !is ExpressionValueArgument
-        ) return
+//        val arguments = resolvedCall.valueArgumentsByIndex
+        val arguments = resolvedCall.argumentMapping.keys.toList()
+//        val arguments = resolvedCall.argumentMapping.values.toList()
+//        if (arguments.isNullOrEmpty()
+//            || arguments[0] == null
+//            || arguments[0] !is ExpressionValueArgument
+//        ) return
 
         /* Last Call Expression is always the call itself, so we drop it */
         val callExpressions = callExpression.collectDescendantsOfType<KtCallExpression>().dropLast(1)
         if (callExpressions.isNotEmpty()
-            && callExpressions.all { it.getResolvedCall(bindingContext)?.resultingDescriptor?.isSuspend == true }
+            && callExpressions.all {
+                (it.resolveToCall()?.singleFunctionCallOrNull()?.partiallyAppliedSymbol?.signature?.symbol as? KaNamedFunctionSymbol)?.isSuspend == true
+//                it.getResolvedCall(bindingContext)?.resultingDescriptor?.isSuspend == true
+            }
         ) {
-            val argExpr = (arguments[0] as ExpressionValueArgument).valueArgument?.asElement() ?: return
+//            val argExpr = (arguments[0] as ExpressionValueArgument).valueArgument?.asElement() ?: return
+            val argExpr = arguments[0]
             kotlinFileContext.reportIssue(argExpr, "Remove this dispatcher. It is pointless when used with only suspending functions.")
         }
     }
