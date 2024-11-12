68dfe12ff9424086b1a69d44226c77f871727b64
Marharyta Nedzelska <margarita.nedzelska@sonarsource.com>
2024-11-12 16:53:38 +0100

    Migrate AndroidBroadcastingCheck

--- sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/AndroidBroadcastingCheck.kt
+++ sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/AndroidBroadcastingCheck.kt
@@ -19,14 +19,16 @@
  */
 package org.sonarsource.kotlin.checks
 
+import org.jetbrains.kotlin.analysis.api.resolution.KaFunctionCall
+import org.jetbrains.kotlin.analysis.api.resolution.symbol
+import org.jetbrains.kotlin.analysis.api.symbols.name
 import org.jetbrains.kotlin.psi.KtCallExpression
-import org.jetbrains.kotlin.resolve.BindingContext
-import org.jetbrains.kotlin.resolve.calls.model.ResolvedCall
-import org.jetbrains.kotlin.resolve.calls.model.ResolvedValueArgument
+import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.psi.psiUtil.isNull
 import org.sonar.check.Rule
 import org.sonarsource.kotlin.api.checks.CallAbstractCheck
 import org.sonarsource.kotlin.api.checks.FunMatcher
-import org.sonarsource.kotlin.api.checks.isNull
+import org.sonarsource.kotlin.api.checks.FunMatcherImpl
 import org.sonarsource.kotlin.api.frontend.KotlinFileContext
 
 private const val MESSAGE = "Make sure that broadcasting intents is safe here."
@@ -38,7 +40,6 @@ private val STICKY_BROADCAST_NAMES = setOf(
     "sendStickyOrderedBroadcastAsUser",
 )
 
-@org.sonarsource.kotlin.api.frontend.K1only("bindingContext")
 @Rule(key = "S5320")
 class AndroidBroadcastingCheck : CallAbstractCheck() {
 
@@ -55,40 +56,38 @@ class AndroidBroadcastingCheck : CallAbstractCheck() {
         )
     })
 
-    // TODO easy?
     override fun visitFunctionCall(
         callExpression: KtCallExpression,
-        resolvedCall: ResolvedCall<*>,
+        resolvedCall: KaFunctionCall<*>,
+        matchedFun: FunMatcherImpl,
         kotlinFileContext: KotlinFileContext
     ) {
-        val arguments = resolvedCall.valueArgumentsByIndex ?: return
-        val name = resolvedCall.resultingDescriptor.name.asString()
+        val arguments = resolvedCall.argumentMapping.keys.toList()
+        val name = resolvedCall.partiallyAppliedSymbol.symbol.name?.asString() ?: return
 
         if (
-            with(kotlinFileContext.bindingContext) {
-                name in STICKY_BROADCAST_NAMES
-                    || isSendBroadcast(name, arguments)
-                    || isSendBroadcastAsUser(name, arguments)
-                    || isSendOrderedBroadcast(name, arguments)
-                    || isSendOrderedBroadcastAsUser(name, arguments)
-            }
+            name in STICKY_BROADCAST_NAMES ||
+            isSendBroadcast(name, arguments) ||
+            isSendBroadcastAsUser(name, arguments) ||
+            isSendOrderedBroadcast(name, arguments) ||
+            isSendOrderedBroadcastAsUser(name, arguments)
         ) {
             kotlinFileContext.reportIssue(callExpression.calleeExpression!!, MESSAGE)
         }
     }
 
-    private fun BindingContext.isSendOrderedBroadcastAsUser(
+    private fun isSendOrderedBroadcastAsUser(
         name: String,
-        argumentsByIndex: List<ResolvedValueArgument>
-    ) = name == "sendOrderedBroadcastAsUser" && (argumentsByIndex.getOrNull(2)?.isNull(this) ?: false)
+        argumentsByIndex: List<KtExpression>
+    ) = name == "sendOrderedBroadcastAsUser" && (argumentsByIndex.getOrNull(2)?.isNull() ?: false)
 
-    private fun BindingContext.isSendOrderedBroadcast(name: String, argumentsByIndex: List<ResolvedValueArgument>) =
-        name == "sendOrderedBroadcast" && (argumentsByIndex.getOrNull(1)?.isNull(this) ?: false)
+    private fun isSendOrderedBroadcast(name: String, argumentsByIndex: List<KtExpression>) =
+        name == "sendOrderedBroadcast" && (argumentsByIndex.getOrNull(1)?.isNull() ?: false)
 
-    private fun BindingContext.isSendBroadcastAsUser(name: String, argumentsByIndex: List<ResolvedValueArgument>) =
-        name == "sendBroadcast" && (argumentsByIndex.getOrNull(1)?.isNull(this) ?: true)
+    private fun isSendBroadcastAsUser(name: String, argumentsByIndex: List<KtExpression>) =
+        name == "sendBroadcast" && (argumentsByIndex.getOrNull(1)?.isNull() ?: true)
 
-    private fun BindingContext.isSendBroadcast(name: String, argumentsByIndex: List<ResolvedValueArgument>) =
-        name == "sendBroadcastAsUser" && (argumentsByIndex.getOrNull(2)?.isNull(this) ?: true)
+    private fun isSendBroadcast(name: String, argumentsByIndex: List<KtExpression>) =
+        name == "sendBroadcastAsUser" && (argumentsByIndex.getOrNull(2)?.isNull() ?: true)
 
 }
