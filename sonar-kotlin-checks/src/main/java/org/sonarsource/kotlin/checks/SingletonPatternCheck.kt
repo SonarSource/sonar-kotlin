60dbd3710c5f5c563c1cc2ae037d720257f3082e
Marharyta Nedzelska <margarita.nedzelska@sonarsource.com>
2024-11-13 10:36:08 +0100

    Migrate SingletonPatternCheck

--- sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/SingletonPatternCheck.kt
+++ sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/SingletonPatternCheck.kt
@@ -19,7 +19,9 @@
  */
 package org.sonarsource.kotlin.checks
 
-import org.jetbrains.kotlin.descriptors.ConstructorDescriptor
+import org.jetbrains.kotlin.analysis.api.resolution.successfulFunctionCallOrNull
+import org.jetbrains.kotlin.analysis.api.resolution.symbol
+import org.jetbrains.kotlin.analysis.api.symbols.KaConstructorSymbol
 import org.jetbrains.kotlin.psi.KtBlockExpression
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtClass
@@ -33,23 +35,19 @@ import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.psi.KtPropertyDelegate
 import org.jetbrains.kotlin.psi.allConstructors
 import org.jetbrains.kotlin.psi.psiUtil.isPrivate
-import org.jetbrains.kotlin.resolve.BindingContext
-import org.jetbrains.kotlin.resolve.calls.util.getCall
-import org.jetbrains.kotlin.resolve.calls.util.getResolvedCall
-import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
 import org.sonar.check.Rule
 import org.sonarsource.kotlin.api.checks.AbstractCheck
 import org.sonarsource.kotlin.api.checks.ConstructorMatcher
 import org.sonarsource.kotlin.api.checks.FunMatcher
 import org.sonarsource.kotlin.api.frontend.KotlinFileContext
 import org.sonarsource.kotlin.api.visiting.KtTreeVisitor
+import org.sonarsource.kotlin.api.visiting.analyze
 
 private val lazyInitializationMatcher = FunMatcher(
     name = "lazy",
     definingSupertype = "kotlin"
 )
 
-@org.sonarsource.kotlin.api.frontend.K1only("easy? try next")
 @Rule(key = "S6515")
 class SingletonPatternCheck : AbstractCheck() {
 
@@ -60,13 +58,13 @@ class SingletonPatternCheck : AbstractCheck() {
         if (singletonClassCandidates.isEmpty()) return
 
         val singleConstructorCallExtractor =
-            SingleConstructorCallExtractor(singletonClassCandidates, kotlinFileContext.bindingContext)
+            SingleConstructorCallExtractor(singletonClassCandidates)
         singleConstructorCallExtractor.visitTree(file)
         val singleConstructorCallByClass = singleConstructorCallExtractor.singleConstructorCallByClass
         if (singleConstructorCallByClass.isEmpty()) return
 
         singleConstructorCallByClass.values.stream().filter {
-            isInitializingCall(it) || isLazyInitializingCall(it, kotlinFileContext.bindingContext)
+            isInitializingCall(it) || isLazyInitializingCall(it)
         }.forEach {
             kotlinFileContext.reportIssue(it, "Singleton pattern should use object declarations or expressions.")
         }
@@ -91,10 +89,8 @@ private fun isSingletonClassCandidate(klass: KtClass): Boolean {
     return klass.isPrivate() || (constructors.isNotEmpty() && constructors.all { it.isPrivate() } )
 }
 
-
 private class SingleConstructorCallExtractor(
     private val singletonClassCandidates: MutableSet<String>,
-    private val bindingContext: BindingContext,
 ) : KtTreeVisitor() {
 
     val singleConstructorCallByClass: MutableMap<String, KtCallExpression> = mutableMapOf()
@@ -104,15 +100,15 @@ private class SingleConstructorCallExtractor(
     override fun visitCallExpression(expression: KtCallExpression) {
         if (singletonClassCandidates.isEmpty() || !constructorMatcher.matches(expression)) return
 
-        val constructorDescriptor =
-            bindingContext[BindingContext.RESOLVED_CALL, expression.getCall(bindingContext)]?.resultingDescriptor as? ConstructorDescriptor
-                ?: return
-
-        val fqName = constructorDescriptor.constructedClass.fqNameSafe.asString()
-        singleConstructorCallByClass.put(fqName, expression)?.let {
-            singleConstructorCallByClass.remove(fqName)
-            singletonClassCandidates.remove(fqName)
-            constructorMatcher = ConstructorMatcher { withTypeNames(*singletonClassCandidates.toTypedArray()) }
+        analyze {
+            val kaConstructorSymbol = expression.resolveToCall()?.successfulFunctionCallOrNull()
+                ?.partiallyAppliedSymbol?.symbol as? KaConstructorSymbol ?: return
+            val fqName = kaConstructorSymbol.containingClassId?.asFqNameString() ?: return
+            singleConstructorCallByClass.put(fqName, expression)?.let {
+                singleConstructorCallByClass.remove(fqName)
+                singletonClassCandidates.remove(fqName)
+                constructorMatcher = ConstructorMatcher { withTypeNames(*singletonClassCandidates.toTypedArray()) }
+            }
         }
     }
 }
@@ -122,7 +118,7 @@ private fun isInitializingCall(callExpression: KtCallExpression): Boolean {
     return isStaticProperty(property)
 }
 
-private fun isLazyInitializingCall(callExpression: KtCallExpression, bindingContext: BindingContext): Boolean {
+private fun isLazyInitializingCall(callExpression: KtCallExpression): Boolean {
     val bodyExpression = callExpression.parent as? KtBlockExpression ?: return false
     if (bodyExpression.statements.last() !== callExpression) return false
     val call = (((bodyExpression.parent as? KtFunctionLiteral)
@@ -131,7 +127,7 @@ private fun isLazyInitializingCall(callExpression: KtCallExpression, bindingCont
         ?.parent as? KtCallExpression ?: return false
 
     val property = (call.parent as? KtPropertyDelegate)?.parent as? KtProperty ?: return false
-    return isStaticProperty(property) && lazyInitializationMatcher.matches(call.getResolvedCall(bindingContext))
+    return isStaticProperty(property) && lazyInitializationMatcher.matches(call)
 }
 
 private fun isStaticProperty(property: KtProperty): Boolean =
