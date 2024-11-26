7c9193cef40b4960ca595ce1f5ba8bea26129f5d
Evgeny Mandrikov <138671+Godin@users.noreply.github.com>
2024-11-26 11:17:54 +0100

    Fix formatting of license header

--- sonar-kotlin-checks/src/test/java/org/sonarsource/kotlin/checks/ServerCertificateCheckTest.kt
+++ sonar-kotlin-checks/src/test/java/org/sonarsource/kotlin/checks/ServerCertificateCheckTest.kt
@@ -1,19 +1,19 @@
 /*
  * SonarSource Kotlin
  * Copyright (C) 2018-2024 SonarSource SA
-  * mailto:info AT sonarsource DOT com
-  *
-  * This program is free software; you can redistribute it and/or
-  * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-  * See the Sonar Source-Available License for more details.
-  *
-  * You should have received a copy of the Sonar Source-Available License
-  * along with this program; if not, see https://sonarsource.com/license/ssal/
-  */
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
 package org.sonarsource.kotlin.checks
 
 class ServerCertificateCheckTest : CheckTestWithNoSemantics(ServerCertificateCheck(), shouldReport = true)
