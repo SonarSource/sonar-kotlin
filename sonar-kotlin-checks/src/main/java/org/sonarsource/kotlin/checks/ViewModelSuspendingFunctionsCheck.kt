ecce185beca8b17aa29ed0faa38bdb8662bd2814
Evgeny Mandrikov <mandrikov@gmail.com>
2024-12-03 23:35:15 +0100

    SONARKT-400 Migrate ViewModelSuspendingFunctionsCheck

--- sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/ViewModelSuspendingFunctionsCheck.kt
+++ sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/ViewModelSuspendingFunctionsCheck.kt
@@ -16,38 +16,34 @@
  */
 package org.sonarsource.kotlin.checks
 
-import org.jetbrains.kotlin.descriptors.ClassDescriptor
+import org.jetbrains.kotlin.analysis.api.symbols.KaClassSymbol
+import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.psiUtil.isPrivate
-import org.jetbrains.kotlin.resolve.BindingContext
-import org.jetbrains.kotlin.resolve.BindingContext.FUNCTION
-import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameOrNull
-import org.jetbrains.kotlin.resolve.descriptorUtil.getAllSuperClassifiers
 import org.sonar.check.Rule
 import org.sonarsource.kotlin.api.checks.AbstractCheck
 import org.sonarsource.kotlin.api.checks.suspendModifier
 import org.sonarsource.kotlin.api.frontend.KotlinFileContext
+import org.sonarsource.kotlin.api.visiting.withKaSession
 
-@org.sonarsource.kotlin.api.frontend.K1only
 @Rule(key = "S6313")
 class ViewModelSuspendingFunctionsCheck : AbstractCheck() {
+    private val viewModelClassId = ClassId.fromString("androidx/lifecycle/ViewModel")
 
     override fun visitNamedFunction(function: KtNamedFunction, kotlinFileContext: KotlinFileContext) {
-        val bindingContext = kotlinFileContext.bindingContext
         function.suspendModifier()?.let {
             if (!function.isPrivate()
-                && function.extendsViewModel(bindingContext)
+                && function.extendsViewModel()
             ) {
                 kotlinFileContext.reportIssue(it,
                     """Classes extending "ViewModel" should not expose suspending functions.""")
             }
         }
     }
-}
 
-private fun KtNamedFunction.extendsViewModel(bindingContext: BindingContext): Boolean {
-    val classDescriptor = bindingContext[FUNCTION, this]?.containingDeclaration as? ClassDescriptor
-    return classDescriptor?.getAllSuperClassifiers()?.any {
-        it.fqNameOrNull()?.asString() == "androidx.lifecycle.ViewModel"
-    } ?: false
+    private fun KtNamedFunction.extendsViewModel(): Boolean = withKaSession {
+        val containingSymbol = symbol.containingSymbol
+        if (containingSymbol !is KaClassSymbol) return false
+        return containingSymbol.superTypes.any { it.isClassType(viewModelClassId) }
+    }
 }
