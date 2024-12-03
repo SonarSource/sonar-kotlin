a92074b6265cbc931ffabb7e85ce20a32c0c039b
Evgeny Mandrikov <mandrikov@gmail.com>
2024-12-03 21:36:56 +0100

    SONARKT-400 Migrate LiftReturnStatementCheck to kotlin-analysis-api

--- sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/LiftReturnStatementCheck.kt
+++ sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/LiftReturnStatementCheck.kt
@@ -27,7 +27,6 @@ import org.sonarsource.kotlin.api.checks.AbstractCheck
 import org.sonarsource.kotlin.api.checks.isExhaustive
 import org.sonarsource.kotlin.api.frontend.KotlinFileContext
 
-@org.sonarsource.kotlin.api.frontend.K1only
 @Rule(key = "S6510")
 class LiftReturnStatementCheck : AbstractCheck() {
 
@@ -47,7 +46,7 @@ class LiftReturnStatementCheck : AbstractCheck() {
             isReturnOrReturnBlock(it.expression!!)
         }
 
-        if (isAllBranchesReturn && expression.isExhaustive(context)) {
+        if (isAllBranchesReturn && expression.isExhaustive()) {
             reportIssue(expression.whenKeyword, context)
         }
     }
