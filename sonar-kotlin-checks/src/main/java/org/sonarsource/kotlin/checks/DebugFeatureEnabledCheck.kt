e0bf3a78a040286f10abc10520c946166278696a
Marharyta Nedzelska <margarita.nedzelska@sonarsource.com>
2024-12-05 00:29:38 +0100

    SONARKT-400 Migrate DebugFeatureEnabledCheck to kotlin-analysis-api

--- sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/DebugFeatureEnabledCheck.kt
+++ sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/DebugFeatureEnabledCheck.kt
@@ -16,18 +16,18 @@
  */
 package org.sonarsource.kotlin.checks
 
+import org.jetbrains.kotlin.analysis.api.resolution.KaFunctionCall
 import org.jetbrains.kotlin.psi.KtCallExpression
-import org.jetbrains.kotlin.resolve.calls.util.getFirstArgumentExpression
-import org.jetbrains.kotlin.resolve.calls.model.ResolvedCall
 import org.sonar.check.Rule
 import org.sonarsource.kotlin.api.checks.CallAbstractCheck
 import org.sonarsource.kotlin.api.checks.FunMatcher
+import org.sonarsource.kotlin.api.checks.FunMatcherImpl
+import org.sonarsource.kotlin.api.checks.getFirstArgumentExpression
 import org.sonarsource.kotlin.api.checks.predictRuntimeBooleanValue
 import org.sonarsource.kotlin.api.frontend.KotlinFileContext
 
 private const val MESSAGE = "Make sure this debug feature is deactivated before delivering the code in production."
 
-@org.sonarsource.kotlin.api.frontend.K1only
 @Rule(key = "S4507")
 class DebugFeatureEnabledCheck : CallAbstractCheck() {
 
@@ -38,8 +38,8 @@ class DebugFeatureEnabledCheck : CallAbstractCheck() {
         { withArguments("kotlin.Boolean") },
     )
 
-    override fun visitFunctionCall(callExpression: KtCallExpression, resolvedCall: ResolvedCall<*>, kotlinFileContext: KotlinFileContext) {
-        if (resolvedCall.getFirstArgumentExpression()?.predictRuntimeBooleanValue(kotlinFileContext.bindingContext) == true) {
+    override fun visitFunctionCall(callExpression: KtCallExpression, resolvedCall: KaFunctionCall<*>, matchedFun: FunMatcherImpl, kotlinFileContext: KotlinFileContext) {
+        if (resolvedCall.getFirstArgumentExpression()?.predictRuntimeBooleanValue() == true) {
             kotlinFileContext.reportIssue(callExpression, MESSAGE)
         }
     }
