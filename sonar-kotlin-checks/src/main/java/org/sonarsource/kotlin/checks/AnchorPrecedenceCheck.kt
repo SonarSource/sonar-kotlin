b53fe0229e93a88f40c1724ba64008891dc82234
Marharyta Nedzelska <margarita.nedzelska@sonarsource.com>
2024-12-05 10:24:02 +0100

    SONARKT-400 Migrate AnchorPrecedenceCheck to kotlin-analysis-api

--- sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/AnchorPrecedenceCheck.kt
+++ sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/AnchorPrecedenceCheck.kt
@@ -22,7 +22,6 @@ import org.sonarsource.analyzer.commons.regex.finders.AnchorPrecedenceFinder
 import org.sonarsource.kotlin.api.regex.AbstractRegexCheck
 import org.sonarsource.kotlin.api.regex.RegexContext
 
-@org.sonarsource.kotlin.api.frontend.K1only
 @Rule(key = "S5850")
 class AnchorPrecedenceCheck : AbstractRegexCheck() {
     override fun visitRegex(regex: RegexParseResult, regexContext: RegexContext) {
