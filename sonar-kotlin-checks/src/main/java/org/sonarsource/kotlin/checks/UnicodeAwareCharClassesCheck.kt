7a08ec25626b2ae8549407014673ca2d62d792e1
Marharyta Nedzelska <margarita.nedzelska@sonarsource.com>
2024-12-05 10:26:09 +0100

    SONARKT-400 Migrate UnicodeAwareCharClassesCheck to kotlin-analysis-api

--- sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/UnicodeAwareCharClassesCheck.kt
+++ sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/UnicodeAwareCharClassesCheck.kt
@@ -26,7 +26,6 @@ import org.sonarsource.kotlin.api.regex.PATTERN_COMPILE_MATCHER
 import org.sonarsource.kotlin.api.regex.RegexContext
 import org.sonarsource.kotlin.api.frontend.KotlinFileContext
 
-@org.sonarsource.kotlin.api.frontend.K1only
 @Rule(key = "S5867")
 class UnicodeAwareCharClassesCheck : AbstractRegexCheck() {
     override fun visitRegex(
