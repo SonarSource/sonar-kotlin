6a5491881ef734c30875c1dacc47f0fe03082bb5
Marharyta Nedzelska <margarita.nedzelska@sonarsource.com>
2024-12-05 10:25:58 +0100

    SONARKT-400 Migrate ReluctantQuantifierCheck to kotlin-analysis-api

--- sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/ReluctantQuantifierCheck.kt
+++ sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/ReluctantQuantifierCheck.kt
@@ -22,7 +22,6 @@ import org.sonarsource.analyzer.commons.regex.finders.ReluctantQuantifierFinder
 import org.sonarsource.kotlin.api.regex.AbstractRegexCheck
 import org.sonarsource.kotlin.api.regex.RegexContext
 
-@org.sonarsource.kotlin.api.frontend.K1only
 @Rule(key = "S5857")
 class ReluctantQuantifierCheck : AbstractRegexCheck() {
     override fun visitRegex(regex: RegexParseResult, regexContext: RegexContext) {
