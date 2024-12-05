70025a799060ce9fe48fa72d19734cd40e3380eb
Marharyta Nedzelska <margarita.nedzelska@sonarsource.com>
2024-12-05 10:24:15 +0100

    SONARKT-400 Migrate DuplicatesInCharacterClassCheck to kotlin-analysis-api

--- sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/DuplicatesInCharacterClassCheck.kt
+++ sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/DuplicatesInCharacterClassCheck.kt
@@ -22,7 +22,6 @@ import org.sonarsource.analyzer.commons.regex.finders.DuplicatesInCharacterClass
 import org.sonarsource.kotlin.api.regex.AbstractRegexCheck
 import org.sonarsource.kotlin.api.regex.RegexContext
 
-@org.sonarsource.kotlin.api.frontend.K1only
 @Rule(key = "S5869")
 class DuplicatesInCharacterClassCheck : AbstractRegexCheck() {
     
