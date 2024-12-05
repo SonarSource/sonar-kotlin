647978ce62df35d68a86f0d159dd42035cacda2b
Marharyta Nedzelska <margarita.nedzelska@sonarsource.com>
2024-12-05 10:25:29 +0100

    SONARKT-400 Migrate RegexComplexityCheck to kotlin-analysis-api

--- sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/RegexComplexityCheck.kt
+++ sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/RegexComplexityCheck.kt
@@ -25,7 +25,6 @@ import org.sonarsource.kotlin.api.regex.RegexContext
 
 private const val DEFAULT_MAX = 20
 
-@org.sonarsource.kotlin.api.frontend.K1only
 @Rule(key = "S5843")
 class RegexComplexityCheck : AbstractRegexCheck() {
 
