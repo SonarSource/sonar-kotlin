ab5bd5ae1efad916b4b4a57e86eeaab72812d1b9
Marharyta Nedzelska <margarita.nedzelska@sonarsource.com>
2024-12-13 09:28:11 +0100

    Add common-custom-user-data-gradle-plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -96,6 +96,7 @@ dependencyResolutionManagement {
 
 plugins {
     id("com.gradle.develocity") version("3.18.2")
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.0.2"
 }
 
 val isCI: Boolean = System.getenv("CI") != null
