7e0607c283fd75df31ea05660d76c2617e9a8b22
Evgeny Mandrikov <138671+Godin@users.noreply.github.com>
2025-01-24 16:41:52 +0100

    SONARKT-423 Migrate RedundantSuspendModifierCheck to kotlin-analysis-api

--- sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/RedundantSuspendModifierCheck.kt
+++ sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/RedundantSuspendModifierCheck.kt
@@ -16,27 +16,31 @@
  */
 package org.sonarsource.kotlin.checks
 
-import org.jetbrains.kotlin.backend.common.descriptors.isSuspend
+import org.jetbrains.kotlin.analysis.api.resolution.successfulFunctionCallOrNull
+import org.jetbrains.kotlin.analysis.api.resolution.symbol
+import org.jetbrains.kotlin.analysis.api.symbols.KaNamedFunctionSymbol
 import org.jetbrains.kotlin.psi.KtNameReferenceExpression
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.psiUtil.findDescendantOfType
-import org.jetbrains.kotlin.resolve.calls.util.getResolvedCall
 import org.sonar.check.Rule
 import org.sonarsource.kotlin.api.checks.AbstractCheck
 import org.sonarsource.kotlin.api.checks.overrides
 import org.sonarsource.kotlin.api.checks.suspendModifier
 import org.sonarsource.kotlin.api.frontend.KotlinFileContext
+import org.sonarsource.kotlin.api.visiting.withKaSession
 
-@org.sonarsource.kotlin.api.frontend.K1only
 @Rule(key = "S6318")
 class RedundantSuspendModifierCheck : AbstractCheck() {
 
-    override fun visitNamedFunction(function: KtNamedFunction, context: KotlinFileContext) {
+    override fun visitNamedFunction(function: KtNamedFunction, context: KotlinFileContext) = withKaSession {
         val suspendModifier = function.suspendModifier() ?: return
         with(function) {
             if (hasBody() && !overrides()) {
                 findDescendantOfType<KtNameReferenceExpression> {
-                    it.getResolvedCall(context.bindingContext)?.resultingDescriptor?.isSuspend ?: true
+                    (it.resolveToCall()?.successfulFunctionCallOrNull()
+                        ?.partiallyAppliedSymbol
+                        ?.symbol as? KaNamedFunctionSymbol)?.isSuspend
+                        ?: true
                 } ?: context.reportIssue(suspendModifier, """Remove this unnecessary "suspend" modifier.""")
             }
         }
