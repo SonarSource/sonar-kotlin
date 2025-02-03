5b5a6adc7a970a856671e7cf2f9f91c038f18c44
Evgeny Mandrikov <mandrikov@gmail.com>
2025-02-03 17:00:35 +0100

    (WIP)

--- sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/PseudoRandomCheck.kt
+++ sonar-kotlin-checks/src/main/java/org/sonarsource/kotlin/checks/PseudoRandomCheck.kt
@@ -16,16 +16,8 @@
  */
 package org.sonarsource.kotlin.checks
 
-import org.jetbrains.kotlin.analysis.api.resolution.singleFunctionCallOrNull
-import org.jetbrains.kotlin.analysis.api.resolution.symbol
-import org.jetbrains.kotlin.analysis.api.signatures.KaCallableSignature
-import org.jetbrains.kotlin.analysis.api.symbols.KaConstructorSymbol
-import org.jetbrains.kotlin.codegen.optimization.common.analyze
-import org.jetbrains.kotlin.descriptors.ConstructorDescriptor
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
-import org.jetbrains.kotlin.resolve.calls.util.getResolvedCall
-import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameOrNull
 import org.sonar.check.Rule
 import org.sonarsource.kotlin.api.checks.AbstractCheck
 import org.sonarsource.kotlin.api.checks.FunMatcher
@@ -40,18 +32,22 @@ private val MATH_RANDOM_MATCHER = FunMatcher(qualifier = "java.lang.Math", name 
 
 private val KOTLIN_RANDOM_MATCHER = FunMatcher (qualifier = "kotlin.random", name = "Random")
 
-private val RANDOM_STATIC_TYPES = setOf(
-    "java.util.concurrent.ThreadLocalRandom",
-    "org.apache.commons.lang.math.RandomUtils",
-    "org.apache.commons.lang3.RandomUtils",
-    "org.apache.commons.lang.RandomStringUtils",
-    "org.apache.commons.lang3.RandomStringUtils",
-)
+private val WIP = FunMatcher {
+   qualifiers = setOf(
+       "java.util.concurrent.ThreadLocalRandom",
+       "org.apache.commons.lang.math.RandomUtils",
+       "org.apache.commons.lang3.RandomUtils",
+       "org.apache.commons.lang.RandomStringUtils",
+       "org.apache.commons.lang3.RandomStringUtils",
+   )
+}
 
-private val RANDOM_CONSTRUCTOR_TYPES = setOf(
-    "java.util.Random",
-    "org.apache.commons.lang.math.JVMRandom"
-)
+private val RANDOM_CONSTRUCTORS_MATCHER = FunMatcher(matchConstructor = true) {
+    qualifiers = setOf(
+        "java.util.Random",
+        "org.apache.commons.lang.math.JVMRandom",
+    )
+}
 
 @Rule(key = "S2245")
 class PseudoRandomCheck : AbstractCheck() {
@@ -62,21 +58,11 @@ class PseudoRandomCheck : AbstractCheck() {
         if (MATH_RANDOM_MATCHER.matches(expression) || KOTLIN_RANDOM_MATCHER.matches(expression))
             kotlinFileContext.reportIssue(calleeExpression, MESSAGE)
 
-        // TODO see similar code in FunMatcher
-        when (val symbol = expression.resolveToCall()?.singleFunctionCallOrNull()?.partiallyAppliedSymbol?.symbol) {
-            null -> {
-            }
-            is KaConstructorSymbol -> {
-                symbol.containingClassId?.asFqNameString()?.let {
-                    if (RANDOM_CONSTRUCTOR_TYPES.contains(it)) kotlinFileContext.reportIssue(calleeExpression, MESSAGE)
-                }
-            }
-            else -> {
-                symbol.callableId?.asSingleFqName()?.parent()?.asString()?.let {
-                    if (RANDOM_STATIC_TYPES.contains(it) && !expression.isChainedMethodInvocation())
-                        kotlinFileContext.reportIssue(calleeExpression, MESSAGE)
-                }
-            }
+        if (RANDOM_CONSTRUCTORS_MATCHER.matches(expression))
+            kotlinFileContext.reportIssue(calleeExpression, MESSAGE)
+
+        if (WIP.matches(expression) && !expression.isChainedMethodInvocation()) {
+            kotlinFileContext.reportIssue(calleeExpression, MESSAGE)
         }
     }
 }
