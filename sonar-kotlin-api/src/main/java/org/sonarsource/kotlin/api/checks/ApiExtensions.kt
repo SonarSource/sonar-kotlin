46ad0d3a653911b5518bfc02206da6afe148d4e7
Evgeny Mandrikov <138671+Godin@users.noreply.github.com>
2025-02-17 11:02:58 +0100

    SONARKT-541 Migrate CollectionShouldBeImmutableCheck to kotlin-analysis-api
    Co-authored-by: Marharyta Nedzelska <margarita.nedzelska@sonarsource.com>
    Co-authored-by: Evgeny Mandrikov <mandrikov@gmail.com>

--- sonar-kotlin-api/src/main/java/org/sonarsource/kotlin/api/checks/ApiExtensions.kt
+++ sonar-kotlin-api/src/main/java/org/sonarsource/kotlin/api/checks/ApiExtensions.kt
@@ -564,7 +564,7 @@ fun KtCallExpression.expressionTypeFqn(bindingContext: BindingContext): String? 
  * > Avoid using [org.jetbrains.kotlin.name.FqName]s or raw strings for type comparison.
  * > Use [org.jetbrains.kotlin.name.ClassId]s instead
  */
-internal fun KaType.asFqNameString(): String? = withKaSession {
+fun KaType.asFqNameString(): String? = withKaSession {
     (lowerBoundIfFlexible() as? KaClassType)?.classId?.asFqNameString()
 }
 
