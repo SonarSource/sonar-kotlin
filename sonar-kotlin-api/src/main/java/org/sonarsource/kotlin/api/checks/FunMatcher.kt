a18f93c06e2ab1b2b49f7e8eaf089528db4d84d5
Antonio Aversa <antonio.aversa@sonarsource.com>
2025-03-27 09:25:34 +0100

    Test FunMatcher behavior with aliased constructors

--- sonar-kotlin-api/src/main/java/org/sonarsource/kotlin/api/checks/FunMatcher.kt
+++ sonar-kotlin-api/src/main/java/org/sonarsource/kotlin/api/checks/FunMatcher.kt
@@ -168,9 +168,7 @@ class FunMatcherImpl(
     /** @return dot-separated package name for top-level functions, class name otherwise */
     private fun getActualQualifier(symbol: KaCallableSymbol): String? {
         return if (symbol is KaConstructorSymbol) {
-            // TODO investigate
-            symbol.returnType.symbol?.classId?.asFqNameString()
-//            symbol.containingClassId?.asFqNameString()
+            symbol.returnType.asFqNameString() // callableId is null for ctors, containingClassId would return type aliases
         } else {
             symbol.callableId?.asSingleFqName()?.parent()?.asString()
         }
