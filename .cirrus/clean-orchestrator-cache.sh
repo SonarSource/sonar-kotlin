5003c3f1645b0aa683f01aee57a7aeab25431813
Evgeny Mandrikov <138671+Godin@users.noreply.github.com>
2025-07-08 10:54:14 +0100

    SONARKT-671 Use lightweight Enterprise SQ edition in integration tests

--- .cirrus/clean-orchestrator-cache.sh
+++ .cirrus/clean-orchestrator-cache.sh
@@ -3,16 +3,16 @@ set -euo pipefail
 
 cd "$ORCHESTRATOR_HOME" || exit 1
 
-# Find all sonar-application-* JAR files, sort them by version, and list them
-files=$(/usr/bin/find . -name 'sonar-application-*' | /usr/bin/sort --version-sort --field-separator=- --key=3 --reverse)
+# Find all sonarqube-enterprise-* JAR files, sort them by version, and list them
+files=$(/usr/bin/find . -name 'sonarqube-enterprise-*' | /usr/bin/sort --version-sort --field-separator=- --key=3 --reverse)
 
 echo "File that will not be deleted:"
 echo "$files" | head -n 1
 
-kotlin_files=$(/usr/bin/find . -name 'sonar-kotlin-plugin-*.jar')
+plugin_files=$(/usr/bin/find . -name 'sonar-kotlin-plugin-*.jar')
 
-# Get the files to delete: all sonar-application except the latest one, and all sonar-kotlin-plugin JAR files
-files_to_delete=$(echo "$files" | tail -n +2; echo "$kotlin_files")
+# Get the files to delete: all sonarqube-enterprise except the latest one, and all sonar-kotlin-plugin JAR files
+files_to_delete=$(echo "$files" | tail -n +2; echo "$plugin_files")
 
 echo ""
 if [ -z "$files_to_delete" ]; then
@@ -21,7 +21,7 @@ else
   echo "Files that will be deleted:"
   echo "$files_to_delete"
 
-  # Delete obsolete sonar-application files
+  # Delete obsolete sonarqube-enterprise files
   # shellcheck disable=SC2016
   echo "$files_to_delete" | xargs -I {} sh -c 'rm -f "{}" && rmdir "$(dirname "{}")" 2>/dev/null || true'
 fi
