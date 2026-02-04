c230ed2a0878ee26aae9d76415072f17af23f603
Peter Trifanov <peter.trifanov@sonarsource.com>
2026-02-04 16:07:11 +0100

    Update Gradle to 9.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -161,13 +161,13 @@ subprojects {
         }
     }
 
-    val sourcesJar by tasks.creating(Jar::class) {
+    val sourcesJar by tasks.registering(Jar::class) {
         dependsOn(JavaPlugin.CLASSES_TASK_NAME)
         archiveClassifier.set("sources")
         from(sourceSets.main.get().allSource)
     }
 
-    val javadocJar by tasks.creating(Jar::class) {
+    val javadocJar by tasks.registering(Jar::class) {
         dependsOn(javadoc)
         archiveClassifier.set("javadoc")
         from(tasks["javadoc"])
@@ -289,13 +289,13 @@ artifactory {
 tasks.register<CreateKotlinRuleStubsTask>("setupRuleStubs") {
     group = "Rules"
     description = "Generate required stubs for a new Kotlin rule"
-    finalizedBy(tasks.findByPath(":generateRuleMetadata"))
+    finalizedBy(tasks.getByPath(":generateRuleMetadata"))
 }
 
 tasks.register<CreateKotlinGradleRuleStubsTask>("setupGradleRuleStubs") {
     group = "Rules"
     description = "Generate required stubs for a new Kotlin Gradle DSL rule"
-    finalizedBy(tasks.findByPath(":generateRuleMetadata"))
+    finalizedBy(tasks.getByPath(":generateRuleMetadata"))
 }
 
 tasks.register<FetchRuleMetadata.FetchSpecificRulesMetadata>("generateRuleMetadata")
