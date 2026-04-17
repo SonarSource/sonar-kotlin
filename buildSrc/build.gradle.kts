afc3aeab879e5ccb75e4f23df262679d6ec53260
Peter Trifanov <peter.trifanov@sonarsource.com>
2026-04-17 17:15:11 +0200

    Exclude bouncycastle from buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,3 +7,10 @@ repositories {
     // Use the plugin portal to apply community plugins in convention plugins.
     gradlePluginPortal()
 }
+
+configurations.matching { it.name == "kotlinBouncyCastleConfiguration" }.configureEach {
+    // Workaround for https://github.com/gradle/gradle/issues/35309.
+    // When any of cloud-native Gradle plugins is applied in a project
+    // whose Gradle version embeds Kotlin <2.3.20, there will be an unnecessary dependency on build classpath.
+    withDependencies { clear() }
+}
