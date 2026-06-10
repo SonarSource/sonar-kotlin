3e1224d10ffaef08607e218bfb902624a5a048f4
Peter Trifanov <peter.trifanov@sonarsource.com>
2026-06-10 15:23:20 +0200

    SONARKT-775 Update Kotlin to 2.4.0 (#732)

--- sonar-kotlin-api/src/main/java/org/sonarsource/kotlin/api/frontend/KotlinCoreEnvironmentTools.kt
+++ sonar-kotlin-api/src/main/java/org/sonarsource/kotlin/api/frontend/KotlinCoreEnvironmentTools.kt
@@ -78,6 +78,7 @@ fun kotlinCoreEnvironment(
     )
 }
 
+@OptIn(CompilerConfiguration.Internals::class)
 fun compilerConfiguration(
     classpath: List<String>,
     languageVersion: LanguageVersion,
