d7b32b3919c027ac20057d0b5d2268cd8fe050e4
Jonas Wielage <jonas.wielage@sonarsource.com>
2026-07-22 15:11:17 +0200

    Bump JDK sourceCompatibility to 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -119,7 +119,7 @@ subprojects {
         skip = true
     }
 
-    java.sourceCompatibility = JavaVersion.VERSION_11
+    java.sourceCompatibility = JavaVersion.VERSION_17
     tasks.withType<JavaCompile> {
         options.encoding = "UTF-8"
         options.release.set(java.sourceCompatibility.majorVersion.toInt())
