d86422560b0f66c23209da776466f3277fe479c7
Jonas Wielage <jonas.wielage@sonarsource.com>
2026-09-09 10:35:58 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -33,7 +33,7 @@ dependencyResolutionManagement {
 
         val kotlinVersion: String by extra
         val analyzerCommonsVersionStr = "2.31.0.5284"
-        val sonarPluginApi = "13.11.0.4459"
+        val sonarPluginApi = "13.4.2.4284"
         val slf4jApi = "1.7.36"
 
         create("libs") {
